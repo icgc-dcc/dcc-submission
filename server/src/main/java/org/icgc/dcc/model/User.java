b2b72b95932dea15a01a1041399e23753ee5b4c6
l2yao <yl1984108@hotmail.com>
2012-05-29 09:30:14 -0400

    DCC-23 minor change to data model, User class should implement HasName interface

--- server/src/main/java/org/icgc/dcc/model/User.java
+++ server/src/main/java/org/icgc/dcc/model/User.java
@@ -6,7 +6,7 @@ import java.util.List;
 import com.google.code.morphia.annotations.Entity;
 
 @Entity
-public class User extends BaseEntity {
+public class User extends BaseEntity implements HasName {
 
   protected String username;
 
@@ -14,5 +14,10 @@ public class User extends BaseEntity {
 
   public boolean hasRole(String role) {
     return this.roles.contains(role);
+  }
+
+  @Override
+  public String getName() {
+    return username;
   }
 }
