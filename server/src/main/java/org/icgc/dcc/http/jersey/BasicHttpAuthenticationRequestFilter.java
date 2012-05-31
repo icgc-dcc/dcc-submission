cc1788cb5d549879b53717e563eb88b4cf48ab62
Philippe Laflamme <philippe.laflamme@oicr.on.ca>
2012-05-31 15:20:10 -0400

    Added Mockito for testing. Added example test

--- server/src/main/java/org/icgc/dcc/http/jersey/BasicHttpAuthenticationRequestFilter.java
+++ server/src/main/java/org/icgc/dcc/http/jersey/BasicHttpAuthenticationRequestFilter.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import javax.ws.rs.BindingPriority;
 import javax.ws.rs.core.RequestHeaders;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.FilterContext;
 import javax.ws.rs.ext.PreMatchRequestFilter;
 import javax.ws.rs.ext.Provider;
@@ -24,25 +22,21 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.net.HttpHeaders;
 
-/* filters are annotated this way in JAX-RS 2.0 */
-@Provider
-/* the lower the number, the higher the priority [https://github.com/resteasy/Resteasy/pull/26/files - TODO: confirm] */
-@BindingPriority(0)
-/* we want it bound to all resources (global binding is default) */
 /**
  * Authentication filter
  * 
- * try it with: $ curl -v -H "Authorization: Basic $(echo -n "brett:brettspasswd" | base64)" http://localhost:5379/ws/myresource
+ * try it with: $ curl -v -H "Authorization: Basic $(echo -n "brett:brettspasswd" | base64)"
+ * http://localhost:5379/ws/myresource
  * 
  * @author Anthony Cros (anthony.cros@oicr.on.ca)
  */
+@Provider
+@BindingPriority(BindingPriority.SECURITY)
 public class BasicHttpAuthenticationRequestFilter implements PreMatchRequestFilter {
 
   private static final Logger log = LoggerFactory.getLogger(BasicHttpAuthenticationRequestFilter.class);
 
   private static final String HTTP_BASIC = "Basic"; // TODO: existing constant somewhere?
-
-  private static final String HTTP_BASIC_REALM = "realm"; // TODO: existing constant somewhere?
 
   private static final String TOKEN_INFO_SEPARATOR = ":";
 
@@ -55,24 +49,26 @@ public class BasicHttpAuthenticationRequestFilter implements PreMatchRequestFilt
 
     // get authorization header
     String authorizationHeader = headers.getHeader(HttpHeaders.AUTHORIZATION);
-    log.info("authorizationHeader = " + authorizationHeader);
+    log.debug("authorizationHeader = " + authorizationHeader);
 
     // check if provided
     if(authorizationHeader == null || authorizationHeader.isEmpty()) {
-      Response response = // TODO: prettify
-          this.buildErrorResponse(Response.Status.UNAUTHORIZED,//
-              HttpHeaders.WWW_AUTHENTICATE, HTTP_BASIC + " " + HTTP_BASIC_REALM + "=" + //
-                  "\"" + WWW_AUTHENTICATE_REALM + "\"");
+      Response response =
+          filterContext
+              .createResponse()
+              .status(Response.Status.UNAUTHORIZED)
+              .header(HttpHeaders.WWW_AUTHENTICATE,
+                  String.format("%s realm=\"%s\"", HTTP_BASIC, WWW_AUTHENTICATE_REALM)).build();
 
-      // does not seem to work for now, probably not implemented in jersey for now (TODO)
+      // TODO: does not seem to work for now, probably not implemented in jersey for now (m04)
       filterContext.setResponse(response);
     } else {
-
       // expected to be like: "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="
       String[] split = authorizationHeader.split(" ", 2);
       if(split.length != 2 || !split[0].equals(HTTP_BASIC)) {
-        Response response = this.buildErrorResponse(Response.Status.BAD_REQUEST); // TODO: add error message?
-        filterContext.setResponse(response); // see comment above
+        // TODO: add error message?
+        Response response = filterContext.createResponse().status(Response.Status.BAD_REQUEST).build();
+        filterContext.setResponse(response);
       } else {
 
         // grabbing the "QWxhZGRpbjpvcGVuIHNlc2FtZQ==" part
@@ -86,7 +82,8 @@ public class BasicHttpAuthenticationRequestFilter implements PreMatchRequestFilt
                                                                          // HttpAuthorizationToken.java
 
         if(decoded.length != 2) {
-          Response response = this.buildErrorResponse(Response.Status.BAD_REQUEST); // TODO: add error message?
+          // TODO: add error message?
+          Response response = filterContext.createResponse().status(Response.Status.BAD_REQUEST).build();
           filterContext.setResponse(response); // see comment above
           // TODO: should we allow using "return;"...?
         } else {
@@ -130,22 +127,5 @@ public class BasicHttpAuthenticationRequestFilter implements PreMatchRequestFilt
       }
     }
 
-  }
-
-  private Response buildErrorResponse(Status status) {
-    return this.buildErrorResponse(status, null, null);
-  }
-
-  private Response buildErrorResponse(Status status, String scheme, String value) { // TODO: should use map (or at least
-                                                                                    // entry) for headers
-    ResponseBuilder responseBuilder = Response.status(status);
-    if(null != scheme) {
-      responseBuilder.header(scheme, value);
-    }
-
-    Response response = responseBuilder.build();
-    log.info("response = " + response.getStatus() + ", " + response.getHeaders().asMap());
-
-    return response;
   }
 }
