8d89462a7e1a31d7ccb6d008d9ffd75754d5a621
l2yao <yl1984108@hotmail.com>
2012-06-05 15:45:28 -0400

    DCC-33 finish up Project Resource, Tested using REST Console

--- server/src/main/java/org/icgc/dcc/web/ReleaseResource.java
+++ server/src/main/java/org/icgc/dcc/web/ReleaseResource.java
@@ -7,7 +7,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -26,7 +25,6 @@ public class ReleaseResource {
   private ReleaseService releaseService;
 
   @GET
-  @Produces("application/json")
   public Response getResources() {
     List<HasRelease> hasReleases = releaseService.list();
     List<Release> releases = new ArrayList<Release>();
