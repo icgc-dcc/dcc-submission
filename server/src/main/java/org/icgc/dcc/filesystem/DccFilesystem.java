818b75ec2b49607e09c759c67c055424de7fda26
anthony.cros@oicr.on.ca <anthony@odl-acros.(none)>
2012-06-06 17:38:24 -0400

    DCC-16 - in progress - mostly worked on DccFilesystem class

--- server/src/main/java/org/icgc/dcc/filesystem/DccFilesystem.java
+++ server/src/main/java/org/icgc/dcc/filesystem/DccFilesystem.java
@@ -2,6 +2,7 @@ package org.icgc.dcc.filesystem;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +10,10 @@ import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.icgc.dcc.config.ConfigConstants;
+import org.icgc.dcc.model.Project;
+import org.icgc.dcc.model.Projects;
+import org.icgc.dcc.model.Release;
+import org.icgc.dcc.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,39 +28,166 @@ public class DccFilesystem {
 
   private final FileSystem fileSystem;
 
-  @Inject
-  public DccFilesystem(Config config, FileSystem fileSystem) {
-    checkArgument(config != null);
-    checkArgument(fileSystem != null);
-    this.config = config;
-    this.fileSystem = fileSystem;
-  }
+  private final Projects projects;
 
-  public void doIt() throws Exception {
-    // dummy
+  private String root;
+
+  @Inject
+  public DccFilesystem(Config config, Projects projects, FileSystem fileSystem) {
+
+    checkArgument(config != null);
+    checkArgument(projects != null);
+    checkArgument(fileSystem != null);
+
+    this.config = config;
+    this.projects = projects;
+    this.fileSystem = fileSystem;
+
     log.info("use_hdfs = " + this.config.getBoolean(ConfigConstants.FS_USE_HDFS));
     log.info("fileSystem = " + this.fileSystem.getClass().getSimpleName());
-
     log.info("home = " + this.fileSystem.getHomeDirectory());
     log.info("wd = " + this.fileSystem.getWorkingDirectory());
-    String root = this.config.getString(ConfigConstants.FS_ROOT_PARAMETER);
-    checkArgument(root != null);
-    log.info("root = " + root);
 
-    Path rootPath = new Path(root);
-    boolean rootExists = this.fileSystem.exists(rootPath);
+    this.mkdirsRootDirectory();
+  }
+
+  /**
+   * Creates root directory if it does not exist
+   */
+  private void mkdirsRootDirectory() {
+
+    // grab root directory
+    this.root = this.config.getString(ConfigConstants.FS_ROOT_PARAMETER);
+    checkArgument(this.root != null);
+    log.info("root = " + this.root);
+
+    // create root dir if it does not exist
+    Path rootPath = new Path(this.root);
+    boolean rootExists = this.checkExistence(rootPath);
     if(!rootExists) {
-      log.info(root + " does not exist");
-      this.fileSystem.mkdirs(rootPath);
-      log.info("created " + root);
+      log.info(this.root + " does not exist");
+      this.createDirectory(rootPath);
+      log.info("created " + this.root);
+    }
+  }
+
+  // TODO: remove dummy
+  public void doIt() throws Exception {
+    Path rootPath = new Path(this.root);
+    log.info("ls = " + this.listContent(rootPath));
+
+    Release myRelease = new Release("ICGC4");
+
+    User myUser = new User();
+    myUser.setUsername("richard");
+
+    this.createReleaseFilesystem(myRelease);
+    log.info("release file system = " + this.getReleaseFilesystem(myRelease, myUser));
+
+    try {
+      this.fileSystem.delete(new Path(this.root + "/" + myRelease.getName()), true);
+    } catch(IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Creates new user-tailored "view" of a given release filesystem. We may change that behavior later to not creating
+   * it on the fly (for now we have very few users and don't plan on having millions ever).
+   */
+  public ReleaseFilesystem getReleaseFilesystem(Release release, User user) {
+    return new ReleaseFilesystem(release, user);
+  }
+
+  /**
+   * Creates the directory arborescence representing given release
+   */
+  public void createReleaseFilesystem(Release release) {
+
+    // create path for release
+    String releaseStringPath = this.buildReleaseStringPath(release);
+    log.info("release path = " + releaseStringPath);
+    Path releasePath = new Path(releaseStringPath);
+
+    // check for pre-existence
+    boolean exists = this.checkExistence(releasePath);
+    if(exists) {
+      throw new DccFileSystemException(this.fileSystem, this.root);
     }
 
-    FileStatus[] listStatus = this.fileSystem.listStatus(rootPath); // non-recursive
+    // create corresponding release directory
+    this.createDirectory(releasePath);
+    checkArgument(this.checkExistence(releasePath)); // TODO: better assert somewhere?
+
+    // create sub-directory for each project
+    List<Project> projectList = this.projects.listProjects();
+    for(Project project : projectList) {
+
+      // create path for project within the release
+      String projectStringPath = this.buildProjectStringPath(releaseStringPath, project);
+      log.info("\t" + "project path = " + projectStringPath);
+      Path projectPath = new Path(projectStringPath);
+      checkArgument(!this.checkExistence(projectPath)); // theoretically can't really happen since we throw an exception
+                                                        // if the
+                                                        // release already exists and we assume all projects have unique
+                                                        // names... TODO?
+
+      // create corresponding project directory
+      this.createDirectory(projectPath);
+      checkArgument(this.checkExistence(projectPath));
+    }
+
+    // log resulting sub-directories
+    log.info("ls " + releaseStringPath + " = " + this.listContent(releasePath));
+  }
+
+  private String buildReleaseStringPath(Release release) {
+    return this.root + "/" + release.getName();
+  }
+
+  private String buildProjectStringPath(String releaseStringPath, Project project) {
+    return releaseStringPath + "/" + project.getName();
+  }
+
+  // TODO: move those methods to HadoopUtils?
+
+  private void createDirectory(Path path) {
+    boolean mkdirs;
+    try {
+      mkdirs = this.fileSystem.mkdirs(path);
+    } catch(IOException e) {
+      throw new DccFileSystemException(this.fileSystem, this.root);
+    }
+    if(!mkdirs) {
+      throw new DccFileSystemException(this.fileSystem, this.root);
+    }
+  }
+
+  private boolean checkExistence(Path releasePath) {
+    boolean exists;
+    try {
+      exists = this.fileSystem.exists(releasePath);
+    } catch(IOException e) {
+      throw new DccFileSystemException(this.fileSystem, this.root);
+    }
+    return exists;
+  }
+
+  /**
+   * non-recursively
+   */
+  private List<String> listContent(Path path) {
+    FileStatus[] listStatus;
+    try {
+      listStatus = this.fileSystem.listStatus(path);
+    } catch(IOException e) {
+      throw new DccFileSystemException(this.fileSystem, this.root);
+    }
     List<String> ls = new ArrayList<String>();
     for(FileStatus fileStatus : listStatus) {
       String name = fileStatus.getPath().getName();
       ls.add(name);
     }
-    log.info("ls = " + ls);
+    return ls;
   }
 }
--- server/src/main/java/org/icgc/dcc/filesystem/SubmissionDirectory.java
+++ server/src/main/java/org/icgc/dcc/filesystem/SubmissionDirectory.java
@@ -1,5 +1,52 @@
 package org.icgc.dcc.filesystem;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.regex.Pattern;
+
+import org.icgc.dcc.model.Project;
+import org.icgc.dcc.model.Release;
+
+import com.google.inject.Inject;
+
 public class SubmissionDirectory {
+
+  @SuppressWarnings("unused")
+  private final Release release;
+
+  @SuppressWarnings("unused")
+  private final Project project;
+
+  private final ReleaseFilesystem parent;
+
+  @Inject
+  public SubmissionDirectory(ReleaseFilesystem parent, Release release, Project project) {
+
+    checkArgument(parent != null);
+    checkArgument(release != null);
+    checkArgument(project != null);
+
+    this.parent = parent;
+    this.release = release;
+    this.project = project;
+  }
+
+  public Iterable<File> listFile(Pattern pattern) {
+    return null;
+  }
+
+  public void addFile(InputStream data) {
+
+  }
+
+  public void deleteFile(String filename) {
+
+  }
+
+  public boolean isReadOnly() {
+    return this.parent.isReadOnly();
+  }
 
 }
