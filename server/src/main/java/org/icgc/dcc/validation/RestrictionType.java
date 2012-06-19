1dfa9d0aafc7cd9e3e6c572920e677e7ac5e8208
Philippe Laflamme <philippe.laflamme@oicr.on.ca>
2012-06-19 14:49:03 -0400

    Cleanup

---
+++ server/src/main/java/org/icgc/dcc/validation/RestrictionType.java
@@ -0,0 +1,16 @@
+package org.icgc.dcc.validation;
+
+import org.icgc.dcc.model.dictionary.Field;
+import org.icgc.dcc.model.dictionary.Restriction;
+
+public interface RestrictionType {
+
+  public String getType();
+
+  public boolean builds(String type);
+
+  public FieldRestrictionTypeSchema getSchema();
+
+  public PipeExtender build(Field field, Restriction restriction);
+
+}
