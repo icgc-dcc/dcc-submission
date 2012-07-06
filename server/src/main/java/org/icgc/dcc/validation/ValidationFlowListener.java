8682c96c20fe20abe4be9fa3f0e7abd14edf0f14
Anthony Cros <anthony.cros@oicr.on.ca>
2012-07-06 09:58:27 -0400

    DCC-118 - in progress

--- server/src/main/java/org/icgc/dcc/validation/ValidationFlowListener.java
+++ server/src/main/java/org/icgc/dcc/validation/ValidationFlowListener.java
@@ -19,6 +19,9 @@ package org.icgc.dcc.validation;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cascading.flow.Flow;
 import cascading.flow.FlowListener;
 import cascading.stats.FlowStats;
@@ -28,12 +31,16 @@ import cascading.stats.FlowStats;
  */
 public class ValidationFlowListener implements FlowListener {
 
+  private static final Logger log = LoggerFactory.getLogger(ValidationFlowListener.class);
+
+  @SuppressWarnings("rawtypes")
   private final List<Flow> flows;
 
   private final String projectKey;
 
   private final ValidationCallback callback;
 
+  @SuppressWarnings("rawtypes")
   ValidationFlowListener(ValidationCallback callback, List<Flow> flows, String projectKey) {
     this.flows = flows;
     this.projectKey = projectKey;
@@ -71,11 +78,13 @@ public class ValidationFlowListener implements FlowListener {
   @Override
   @SuppressWarnings("rawtypes")
   public void onStopping(Flow flow) {
+    log.info("stopping flow {}", flow.getName());
   }
 
   @Override
   @SuppressWarnings("rawtypes")
   public boolean onThrowable(Flow flow, Throwable t) {
+    log.info("error in flow {}: {}", flow.getName(), t.getMessage());
     return false;
   }
 }
