7966f5c295ecb18694ebf7407d8a6be4b261536b
Long Yao <lyao@oicr.on.ca>
2012-07-18 16:54:10 -0400

    add jersey test framework to pom, and create a simple integration test for testing webresources

---
+++ server/src/test/java/org/icgc/dcc/integration/IntegrationTest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2012(c) The Ontario Institute for Cancer Research. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.icgc.dcc.integration;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.external.ExternalTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainerException;
+import org.glassfish.jersey.test.spi.TestContainerFactory;
+import org.icgc.dcc.web.ReleaseResource;
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class IntegrationTest extends JerseyTest {
+
+  @Test
+  public void test() {
+    WebTarget target = this.target();
+    Response response = target.path("releases").request(MediaType.APPLICATION_JSON).get();
+    assertEquals(200, response.getStatus());
+  }
+
+  @Override
+  protected ResourceConfig configure() {
+    return new ResourceConfig().addClasses(ReleaseResource.class);
+  }
+
+  @Override
+  protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
+    return new ExternalTestContainerFactory();
+  }
+
+}
