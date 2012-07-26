a24faf038eece66a06ca286c5ade63f2597dce09
Philippe Laflamme <philippe.laflamme@oicr.on.ca>
2012-07-26 11:36:59 -0400

    Refactoring CascadingStrategy to use a common base class based on FileSystem

---
+++ server/src/main/java/org/icgc/dcc/validation/BaseCascadingStrategy.java
@@ -0,0 +1,123 @@
+/**
+ * Copyright 2012(c) The Ontario Institute for Cancer Research. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.icgc.dcc.validation;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.icgc.dcc.dictionary.model.FileSchema;
+
+import cascading.tap.Tap;
+import cascading.tuple.Fields;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+import com.google.common.io.Closeables;
+import com.google.common.io.LineReader;
+
+public abstract class BaseCascadingStrategy implements CascadingStrategy {
+
+  private final FileSystem fileSystem;
+
+  private final Path input;
+
+  private final Path output;
+
+  protected BaseCascadingStrategy(FileSystem fileSystem, Path input, Path output) {
+    this.fileSystem = fileSystem;
+    this.input = input;
+    this.output = output;
+  }
+
+  @Override
+  public Tap<?, ?, ?> getSourceTap(FileSchema schema) {
+    try {
+      return tapSource(path(schema));
+    } catch(IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Tap<?, ?, ?> getFlowSinkTap(FileSchema schema, FlowType flowType) {
+    return tap(new Path(output, String.format("%s.%s.tsv", schema.getName(), flowType)));
+  }
+
+  @Override
+  public Tap<?, ?, ?> getTrimmedTap(Trim trim) {
+    return tap(trimmedPath(trim), new Fields(trim.getFields()));
+  }
+
+  @Override
+  public InputStream readReportTap(FileSchema schema, FlowType type, String reportName) throws IOException {
+    Path reportPath = reportPath(schema, type, reportName);
+    return fileSystem.open(reportPath);
+  }
+
+  @Override
+  public Fields getFileHeader(FileSchema schema) throws IOException {
+    Path path = path(schema);
+    InputStreamReader isr = null;
+    try {
+      isr = new InputStreamReader(fileSystem.open(path), Charsets.UTF_8);
+      LineReader lineReader = new LineReader(isr);
+      String firstLine = lineReader.readLine();
+      Iterable<String> header = Splitter.on('\t').split(firstLine);
+      return new Fields(Iterables.toArray(header, String.class));
+    } finally {
+      Closeables.closeQuietly(isr);
+    }
+  }
+
+  protected Path trimmedPath(Trim trim) {
+    return new Path(output, trim.getSchema() + "#" + Joiner.on("-").join(trim.getFields()) + ".tsv");
+  }
+
+  protected Path reportPath(FileSchema schema, FlowType type, String reportName) {
+    return new Path(output, String.format("%s.%s#%s.json", schema.getName(), type, reportName));
+  }
+
+  protected abstract Tap<?, ?, ?> tap(Path path);
+
+  protected abstract Tap<?, ?, ?> tap(Path path, Fields fields);
+
+  protected abstract Tap<?, ?, ?> tapSource(Path path);
+
+  private Path path(final FileSchema schema) throws FileNotFoundException, IOException {
+    RemoteIterator<LocatedFileStatus> files = fileSystem.listFiles(input, false);
+    while(files.hasNext()) {
+      LocatedFileStatus file = files.next();
+      if(file.isFile()) {
+        Path path = file.getPath();
+        if(Pattern.matches(schema.getPattern(), path.getName())) {
+          return path;
+        }
+      }
+    }
+    throw new FileNotFoundException("no file for schema " + schema.getName());
+  }
+}
