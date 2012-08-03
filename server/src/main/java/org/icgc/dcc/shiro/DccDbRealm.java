95009954488a1a271628aba6f10bdd95afcbb85c
Jonathan Guberman <jonathan.guberman@oicr.on.ca>
2012-08-03 11:20:27 -0400

    DCC-213 Changes to allow system-wide access filtering by using a custom Shiro Realm
    This commit sets the groundwork, but no actual filtering is in place
    yet. Filtering will be done using Shiro subjects. This will be passed to
    the system using the HTTP SecurityContext. Currently Jersey 2.0 has a
    bug that prevents this from working, but according to the Jersey JIRA it
    was patched two days ago and should be fixed in the release this coming
    Monday.

---
+++ server/src/main/java/org/icgc/dcc/shiro/DccDbRealm.java
@@ -0,0 +1,58 @@
+package org.icgc.dcc.shiro;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.icgc.dcc.core.ProjectService;
+import org.icgc.dcc.core.UserService;
+import org.icgc.dcc.core.model.Project;
+import org.icgc.dcc.core.model.User;
+
+public class DccDbRealm extends AuthorizingRealm {
+
+  private final UserService users;
+
+  private final ProjectService projects;
+
+  public DccDbRealm(UserService users, ProjectService projects) {
+    this.users = users;
+    this.projects = projects;
+  }
+
+  @Override
+  protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+    String username = (String) principals.getPrimaryPrincipal();
+    User user = users.getUser(username);
+    if(user == null) {
+      user = new User();
+      user.setUsername(username);
+      users.saveUser(user);
+    }
+    SimpleAuthorizationInfo sai = new SimpleAuthorizationInfo(new HashSet<String>(user.getRoles()));
+    Set<String> stringPermissions = new HashSet<String>();
+    for(Project project : projects.getProjects()) {
+      if(project.getUsers().contains(user.getName())
+          || CollectionUtils.containsAny(project.getGroups(), user.getRoles())) {
+        stringPermissions.add("project:" + project.getKey());
+      }
+    }
+    sai.addStringPermissions(stringPermissions);
+
+    return sai;
+  }
+
+  @Override
+  protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+    // Authorization is currently only performed via the Shiro INI file, so this is not populated
+    return null;
+  }
+
+}
