2c3517912327f586d2f9005efb66ea5503412a30
Anthony Cros <anthony.cros@oicr.on.ca>
2012-08-09 09:57:57 -0400

    DCC-269 - reprivatized SubmissionFile and introduce structural errors into one of our test data files
    Former-commit-id: 1755f980fd9a2b22872e75a5d467a579dc4f585c
    Former-commit-id: b5bed400034f501116b9d37e3592a85d2e7ed2fc

--- server/src/main/java/org/icgc/dcc/filesystem/SubmissionFile.java
+++ server/src/main/java/org/icgc/dcc/filesystem/SubmissionFile.java
@@ -5,19 +5,26 @@ import java.util.Date;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.icgc.dcc.filesystem.hdfs.HadoopUtils;
 
 /**
  * For serializing file data through the REST interface
  */
 public class SubmissionFile {
-  public String name;
+  private final String name;
 
-  public Date lastUpdate;
+  private final Date lastUpdate;
 
-  public long size;
+  private final long size;
 
-  public SubmissionFile() {
+  @JsonCreator
+  public SubmissionFile(@JsonProperty("name") String name, @JsonProperty("lastUpdate") Date lastUpdate,
+      @JsonProperty("size") long size) {
+    this.name = name;
+    this.lastUpdate = lastUpdate;
+    this.size = size;
   }
 
   public SubmissionFile(Path path, FileSystem fs) {
@@ -26,5 +33,17 @@ public class SubmissionFile {
     FileStatus fileStatus = HadoopUtils.getFileStatus(fs, path);
     this.lastUpdate = new Date(fileStatus.getModificationTime());
     this.size = fileStatus.getLen();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Date getLastUpdate() {
+    return lastUpdate;
+  }
+
+  public long getSize() {
+    return size;
   }
 }
