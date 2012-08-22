d004b4d2c54425d8126692e7e839569c4eb95c84
Philippe Laflamme <philippe.laflamme@oicr.on.ca>
2012-08-22 11:39:13 -0400

    Refactoring BaseRelease, CompletedRelease and NextRelease
    Former-commit-id: 0c78f0a0faeb3f3149a67c6aca3ce1524efb717f
    Former-commit-id: 0b7ba6f087dc301796276ab881859775a526e84e

--- server/src/main/java/org/icgc/dcc/release/HasRelease.java
+++ server/src/main/java/org/icgc/dcc/release/HasRelease.java
@@ -1,5 +1,8 @@
 package org.icgc.dcc.release;
 
+import java.util.List;
+
+import org.icgc.dcc.core.model.Project;
 import org.icgc.dcc.core.model.User;
 import org.icgc.dcc.filesystem.ReleaseFileSystem;
 import org.icgc.dcc.release.model.Release;
@@ -8,5 +11,9 @@ public interface HasRelease {
 
   public ReleaseFileSystem getReleaseFilesystem(User user);
 
+  public ReleaseFileSystem getReleaseFilesystem();
+
   public Release getRelease();
+
+  public List<Project> getProjects();
 }
