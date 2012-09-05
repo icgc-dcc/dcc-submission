49bb4b3d949d59d313d96029dddbcb6f6ce668c3
Shane Wilson <shane.wilson@oicr.on.ca>
2012-09-05 12:36:53 -0400

    DCC-369 Starts logic for aggregated error report
    Former-commit-id: 9afbe4586803591d72a967bd4cac6821dabaa783
    Former-commit-id: c74912872a414da0e72a7a3ce011e0abaa83c0eb

--- server/src/main/java/org/icgc/dcc/validation/report/ErrorPlanningVisitor.java
+++ server/src/main/java/org/icgc/dcc/validation/report/ErrorPlanningVisitor.java
@@ -21,6 +21,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.codehaus.jackson.map.MappingIterator;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -30,6 +33,7 @@ import org.icgc.dcc.validation.FlowType;
 import org.icgc.dcc.validation.PlanExecutionException;
 import org.icgc.dcc.validation.ReportingFlowPlanningVisitor;
 import org.icgc.dcc.validation.ReportingPlanElement;
+import org.icgc.dcc.validation.ValidationErrorCode;
 import org.icgc.dcc.validation.cascading.TupleState;
 import org.icgc.dcc.validation.cascading.TupleStates;
 import org.icgc.dcc.validation.cascading.ValidationFields;
@@ -93,11 +97,17 @@ public class ErrorPlanningVisitor extends ReportingFlowPlanningVisitor {
 
     class ErrorReportCollector implements ReportCollector {
 
+      private final Map<ValidationErrorCode, ValidationErrorReport> errorMap =
+          new HashMap<ValidationErrorCode, ValidationErrorReport>();
+
       public ErrorReportCollector() {
       }
 
       @Override
       public Outcome collect(CascadingStrategy strategy, SchemaReport report) {
+
+        System.out.println("report: " + report);
+
         InputStream src = null;
         try {
           src = strategy.readReportTap(getFileSchema(), getFlowType(), getName());
@@ -112,20 +122,46 @@ public class ErrorPlanningVisitor extends ReportingFlowPlanningVisitor {
             report.setFieldReports(new ArrayList<FieldReport>());
           }
 
+          System.out.println("report name: " + report.getName());
+
           Outcome outcome = Outcome.PASSED;
           MappingIterator<TupleState> tupleStates = mapper.reader().withType(TupleState.class).readValues(src);
           while(tupleStates.hasNext()) {
             TupleState tupleState = tupleStates.next();
             if(tupleState.isInvalid()) {
               outcome = Outcome.FAILED;
-              ValidationErrorReport aggregatedErrorReport = new ValidationErrorReport(tupleState);
-              report.errors.add(aggregatedErrorReport);
+              // ValidationErrorReport aggregatedErrorReport = new ValidationErrorReport(tupleState);
+              // report.errors.add(aggregatedErrorReport);
+
+              // Loop thought the errors
+              Iterator<TupleState.TupleError> errors = tupleState.getErrors().iterator();
+              while(errors.hasNext()) {
+                TupleState.TupleError error = errors.next();
+                System.out.println("errorMap: " + errorMap);
+                // check if errorMap[error.getCode()] exists
+                if(errorMap.containsKey(error.getCode()) == false) {
+                  // if it does NOT - create it as a new ValidationErrorReport
+                  ValidationErrorReport errorReport = new ValidationErrorReport(error);
+                  errorMap.put(error.getCode(), errorReport);
+                } else {
+                  // if it does
+                  // check if the columnName is already there
+                  // if it is NOT push params to columns list
+                  // if it is - update it by pushing offset/vals and count++
+                }
+              }
             }
             if(report.getErrors().size() >= 100) {
               // Limit to 100 errors
               break;
             }
           }
+          if(errorMap.isEmpty() == false) {
+            for(ValidationErrorReport e : errorMap.values()) {
+              report.errors.add(e);
+            }
+          }
+          System.out.println("report errors: " + report.getErrors());
           return outcome;
         } catch(FileNotFoundException fnfe) {
           return Outcome.PASSED;
