dc541b4115c27529077499224f4074e50b2303ea
Anthony Cros <anthony.cros@oicr.on.ca>
2012-10-30 16:21:39 -0400

    DCC-571 - some grizzly setup fiddling
    Former-commit-id: a8fc1590a220ffbee7880bb4d33dddf3c283521d
    Former-commit-id: eb0693eea24f7c3329f5cdf2f3922625977e14c2

--- server/src/main/java/org/icgc/dcc/http/HttpServerService.java
+++ server/src/main/java/org/icgc/dcc/http/HttpServerService.java
@@ -3,9 +3,14 @@ package org.icgc.dcc.http;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Set;
 
 import javax.inject.Inject;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.HttpServer;
@@ -14,6 +19,7 @@ import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
 import org.glassfish.grizzly.http.server.ServerConfiguration;
 import org.glassfish.grizzly.http.server.StaticHttpHandler;
+import org.glassfish.grizzly.ssl.SSLEngineConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,13 +55,29 @@ public class HttpServerService extends AbstractService {
     final String host = config.getString("http.listen");
     final int port = config.getInt("http.port");
     final Set<String> resources = ImmutableSet.copyOf(config.getStringList("http.resources"));
-    server.addListener(new NetworkListener("http", host, port));
 
+    // add network listener
+    NetworkListener networkListener = new NetworkListener("dcc_https_listener", host, port); // http://grizzly.java.net/nonav/docs/2.0/apidocs/org/glassfish/grizzly/http/server/NetworkListener.html
+
+    SSLEngineConfigurator sslEngineConfigurator = null;
+    try {
+      sslEngineConfigurator = createSSLEngineConfigurator();
+    } catch(NoSuchAlgorithmException e) {
+      e.printStackTrace(); // TODO
+    } catch(KeyManagementException e) {
+      e.printStackTrace(); // TODO
+    }
+
+    networkListener.setSecure(true);
+    networkListener.setSSLEngineConfig(sslEngineConfigurator);
+
+    server.addListener(networkListener);
+
+    // add the http handlers
     final ServerConfiguration serverConfig = server.getServerConfiguration();
     for(HttpHandlerProvider provider : handlerProviders) {
       serverConfig.addHttpHandler(provider.get(), provider.path());
     }
-
     serverConfig.addHttpHandler(new StaticHttpHandler(resources), "/");
 
     // Redirect back to "/" and appends the request url after the hash(#), which the client can then parse
@@ -73,6 +95,19 @@ public class HttpServerService extends AbstractService {
       log.error("Failed to start HTTP server on {}:{} : {}", new Object[] { host, port, ex.getMessage() });
       notifyFailed(ex);
     }
+  }
+
+  private SSLEngineConfigurator createSSLEngineConfigurator() throws NoSuchAlgorithmException, KeyManagementException {
+    KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(//
+        KeyManagerFactory.getDefaultAlgorithm()); // Prepare a key manager using the provided keystore
+
+    TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(//
+        TrustManagerFactory.getDefaultAlgorithm());
+
+    SSLContext sslContext = SSLContext.getInstance("TLS");
+    sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+    return new SSLEngineConfigurator(sslContext, false, false, false);
   }
 
   @Override
