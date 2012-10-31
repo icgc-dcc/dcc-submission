f2937bd7e56cc0d23d0cd2b8c27dd7a5bcc6e235
Anthony Cros <anthony.cros@oicr.on.ca>
2012-10-31 11:12:19 -0400

    DCC-571 - code cleanup
    Former-commit-id: 01a79f5c4d9072f7a66cbe2a2a39501ea1d7fcfd
    Former-commit-id: 70975017287492c694bccf042809120bd13d6c28

--- server/src/main/java/org/icgc/dcc/http/HttpServerService.java
+++ server/src/main/java/org/icgc/dcc/http/HttpServerService.java
@@ -2,9 +2,15 @@ package org.icgc.dcc.http;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -34,6 +40,14 @@ public class HttpServerService extends AbstractService {
 
   private static final Logger log = LoggerFactory.getLogger(HttpServerService.class);
 
+  private static final String LISTENER_NAME = "dcc_https_listener";
+
+  private static final String PROTOCOL = "TLS";
+
+  private static final String CERTIFICATE = "/keystore";
+
+  private static final String CERTIFICATE_PASSWORD = "tmptmptmp"; // FIXME
+
   private final Config config;
 
   private final HttpServer server;
@@ -52,25 +66,19 @@ public class HttpServerService extends AbstractService {
 
   @Override
   protected void doStart() {
+
     final String host = config.getString("http.listen");
     final int port = config.getInt("http.port");
+    final boolean useSsl = true;// config.getBoolean("http.ssl");
     final Set<String> resources = ImmutableSet.copyOf(config.getStringList("http.resources"));
+    log.info("host = {}, port = {}, use SSL = {}, resources = {}", new Object[] { host, port, useSsl, resources });
 
     // add network listener
-    NetworkListener networkListener = new NetworkListener("dcc_https_listener", host, port); // http://grizzly.java.net/nonav/docs/2.0/apidocs/org/glassfish/grizzly/http/server/NetworkListener.html
-
-    SSLEngineConfigurator sslEngineConfigurator = null;
-    try {
-      sslEngineConfigurator = createSSLEngineConfigurator();
-    } catch(NoSuchAlgorithmException e) {
-      e.printStackTrace(); // TODO
-    } catch(KeyManagementException e) {
-      e.printStackTrace(); // TODO
+    NetworkListener networkListener = new NetworkListener(LISTENER_NAME, host, port);
+    if(useSsl) {
+      networkListener.setSecure(true);
+      networkListener.setSSLEngineConfig(createSSLEngineConfigurator());
     }
-
-    networkListener.setSecure(true);
-    networkListener.setSSLEngineConfig(sslEngineConfigurator);
-
     server.addListener(networkListener);
 
     // add the http handlers
@@ -97,17 +105,67 @@ public class HttpServerService extends AbstractService {
     }
   }
 
-  private SSLEngineConfigurator createSSLEngineConfigurator() throws NoSuchAlgorithmException, KeyManagementException {
-    KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(//
-        KeyManagerFactory.getDefaultAlgorithm()); // Prepare a key manager using the provided keystore
+  /**
+   * Creates {@code SSLEngineConfigurator} object necessary to configure self-signed certificate for SSL.
+   */
+  private SSLEngineConfigurator createSSLEngineConfigurator() {
+    SSLEngineConfigurator sslEngineConfigurator = null;
+    try {
+      char[] password = CERTIFICATE_PASSWORD.toCharArray();
+      KeyStore keyStore = createKeyStore(password);
 
-    TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(//
-        TrustManagerFactory.getDefaultAlgorithm());
+      KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+      keyManagerFactory.init(keyStore, password);
 
-    SSLContext sslContext = SSLContext.getInstance("TLS");
-    sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+      TrustManagerFactory trustManagerFactory =
+          TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+      trustManagerFactory.init(keyStore);
 
-    return new SSLEngineConfigurator(sslContext, false, false, false);
+      SSLContext sslContext = SSLContext.getInstance(PROTOCOL);
+      sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+      sslEngineConfigurator = new SSLEngineConfigurator(sslContext, false, false, false);
+    } catch(UnrecoverableKeyException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch(KeyStoreException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch(FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch(CertificateException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch(IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch(NoSuchAlgorithmException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch(KeyManagementException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return sslEngineConfigurator;
+  }
+
+  /**
+   * Creates a {@code KeyStore} to point to our self-signed certificate.
+   */
+  private KeyStore createKeyStore(char[] password) throws KeyStoreException, IOException, NoSuchAlgorithmException,
+      CertificateException {
+    log.info("using certificate: {}", CERTIFICATE);
+    KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+    InputStream is = this.getClass().getResourceAsStream(CERTIFICATE);
+    if(is == null) {
+      log.error("cannot find: {}", CERTIFICATE);
+      throw new RuntimeException(CERTIFICATE);// TODO
+    }
+    keyStore.load(is, password);
+    is.close();
+    return keyStore;
   }
 
   @Override
