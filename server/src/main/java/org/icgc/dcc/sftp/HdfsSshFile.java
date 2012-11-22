7df16decfb5554502aecf8260abff4d6002c8bce
Long Yao <lyao@oicr.on.ca>
2012-11-22 10:35:18 -0500

    add comments on why we use IOException for sftp createInputStream
    Former-commit-id: 3ac01fd5192e07b4ab2e8815114bcc5e9753853b
    Former-commit-id: 9163c10b93581a3d36c209e0482feb6436c264e7

--- server/src/main/java/org/icgc/dcc/sftp/HdfsSshFile.java
+++ server/src/main/java/org/icgc/dcc/sftp/HdfsSshFile.java
@@ -156,6 +156,8 @@ abstract class HdfsSshFile implements SshFile {
   public InputStream createInputStream(long offset) throws IOException {
     // because of DCC-412, the download size bug, we will temporarily disable download
     // return fs.open(path);
+    // Ideally we would throw an Unsupported Exception, but mina will kick user out
+    // so we have to use a low level IOException to keep user connected
     throw new IOException("download from SFTP is disabled");
   }
 
