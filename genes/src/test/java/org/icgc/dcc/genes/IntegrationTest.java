a2f74a587c059fa658ec21e46fb85aa4adc09508
btiernay <btiernay@oanda.com>
2013-01-28 07:40:32 -0500

    Added json validation assertion
    Former-commit-id: 9b73f824108b9e60fdc7e1b2dae3ef978e941c45
    Former-commit-id: b370b5828ad6143f92a285f554da3d2c3b92af15

--- genes/src/test/java/org/icgc/dcc/genes/IntegrationTest.java
+++ genes/src/test/java/org/icgc/dcc/genes/IntegrationTest.java
@@ -17,15 +17,82 @@
  */
 package org.icgc.dcc.genes;
 
-import java.io.IOException;
+import static org.fest.assertions.api.Assertions.assertThat;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import com.github.fge.jsonschema.report.ValidationReport;
+import com.github.fge.jsonschema.util.JsonLoader;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.MongoURI;
 
 public class IntegrationTest {
 
+  private JsonSchema schema;
+  
+  @Before
+  public void setUp() throws IOException {
+    this.schema = getSchema();
+  }
+  
   @Test
   public void testSystem() throws IOException {
-    Main.main("-f", "src/test/resources/heliotrope/genes.bson", "-d", "mongodb://localhost/dcc-genes.Genes");
+    String bsonFile = "src/test/resources/heliotrope/genes.bson";
+    String uri = "mongodb://localhost/dcc-genes.Genes";
+    Main.main("-f", bsonFile, "-d", uri);
+
+    JsonNode gene = getGene(uri);
+    ValidationReport report = validate(gene);
+
+    assertThat(report.getMessages()).isEmpty();
+  }
+
+  private ValidationReport validate(JsonNode gene) {
+    ValidationReport report = schema.validate(gene);
+    
+    return report;
+  }
+
+  private JsonNode getGene(String uri) throws UnknownHostException {
+    MongoCollection genes = getGenes(uri);
+    JsonNode gene = genes.findOne().as(JsonNode.class);
+    genes.getDBCollection().getDB().getMongo().close();
+    
+    return gene;
+  }
+
+  private MongoCollection getGenes(String uri) throws UnknownHostException {
+    MongoURI mongoUri = new MongoURI(uri);
+    Jongo jongo = getJongo(mongoUri);
+    MongoCollection genes = jongo.getCollection(mongoUri.getCollection());
+
+    return genes;
+  }
+
+  private Jongo getJongo(MongoURI mongoUri) throws UnknownHostException {
+    Mongo mongo = new Mongo(mongoUri);
+    DB db = mongo.getDB(mongoUri.getDatabase());
+    Jongo jongo = new Jongo(db);
+    
+    return jongo;
+  }
+
+  private JsonSchema getSchema() throws IOException {
+    JsonNode schemaNode = JsonLoader.fromResource("/genes.schema.json");
+    JsonSchemaFactory factory = JsonSchemaFactory.defaultFactory();
+    JsonSchema schema = factory.fromSchema(schemaNode);
+    
+    return schema;
   }
 
 }
