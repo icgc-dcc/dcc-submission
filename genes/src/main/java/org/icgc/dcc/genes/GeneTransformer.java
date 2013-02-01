3392df326886c96acc4d0b5b9d80d3dfdb230cba
Bob Tiernay <btiernay@oicr.on.ca>
2013-02-01 11:16:28 -0500

    Changed conventions for names to be _ seperated words. Added description field. Renamed id fields to be "id". Made ids required.
    Former-commit-id: ab53d4189590eb91ec78629db4c918428395a2ba
    Former-commit-id: 588a9b3b37228d13123efda13c311ab665ac7d85

--- genes/src/main/java/org/icgc/dcc/genes/GeneTransformer.java
+++ genes/src/main/java/org/icgc/dcc/genes/GeneTransformer.java
@@ -34,34 +34,39 @@ public class GeneTransformer {
 
   public JsonNode transform(JsonNode node) {
     ObjectNode result = mapper.createObjectNode();
-    
+
     // Simple
     result.set("id", id(node));
     result.set("symbol", symbol(node));
     result.set("name", name(node));
     result.set("synonyms", synonyms(node));
+    result.set("description", description(node));
     result.set("chromosome", location(node).path("chromosome"));
     result.set("strand", location(node).path("strand"));
     result.set("start", location(node).path("txStart"));
     result.set("end", location(node).path("txEnd"));
     result.set("canonical_transcript_id", canonicalTranscriptId(node));
-    
+
     // Collection
     result.put("transcripts", transcripts(node));
 
     return result;
   }
 
-  private JsonNode name(JsonNode node) {
-    return node.path("sections").path("description").path("data").path("fullName");
+  private JsonNode id(JsonNode node) {
+    return node.path("id");
   }
 
   private JsonNode symbol(JsonNode node) {
     return node.path("name");
   }
 
-  private JsonNode id(JsonNode node) {
-    return node.path("id");
+  private JsonNode name(JsonNode node) {
+    return node.path("sections").path("description").path("data").path("fullName");
+  }
+
+  private JsonNode description(JsonNode node) {
+    return node.path("sections").path("description").path("data").path("description");
   }
 
   private ArrayNode synonyms(JsonNode node) {
@@ -69,7 +74,7 @@ public class GeneTransformer {
     JsonNode synonyms = node.path("sections").path("description").path("data").path("synonyms");
 
     ArrayNode values = mapper.createArrayNode();
-    
+
     // Project additional values
     for(JsonNode synonym : synonyms) {
       String value = synonym.asText();
@@ -91,57 +96,71 @@ public class GeneTransformer {
   }
 
   private ArrayNode transcripts(JsonNode node) {
-    JsonNode transcripts = node.path("sections").path("transcripts").path("data").path("records");
+    JsonNode values = node.path("sections").path("transcripts").path("data").path("records");
 
-    ArrayNode values = mapper.createArrayNode();
-    
+    ArrayNode transcripts = mapper.createArrayNode();
+
     // Project transformed transcripts
-    for(JsonNode record : transcripts) {
-      values.add(transcript(record));
+    for(JsonNode value : values) {
+      transcripts.add(transcript(value));
     }
 
-    return values;
+    return transcripts;
   }
 
-  private JsonNode transcript(JsonNode record) {
+  private JsonNode transcript(JsonNode node) {
     ObjectNode transcript = mapper.createObjectNode();
-    
+
     // Simple
-    transcript.put("transcript_id", record.path("id").asText());
-    transcript.put("transcript_name", record.path("name").asText());
-    transcript.put("isCanonical", record.path("isCanonical").asBoolean());
-    transcript.put("length", record.path("length").asLong());
-    transcript.put("lengthAminoAcid", record.path("lengthAminoAcid").asLong());
-    transcript.put("lengthDNA", record.path("lengthDNA").asLong());
-    transcript.put("numberOfExons", record.path("numberOfExons").asLong());
-    transcript.put("startExon", record.path("startExon").asLong());
-    transcript.put("seqExonStart", record.path("seqExonStart").asLong());
-    transcript.put("seqExonEnd", record.path("seqExonEnd").asLong());
-    transcript.put("endExon", record.path("endExon").asLong());
-    transcript.put("translationId", record.path("translationId").asText());
+    transcript.put("id", node.path("id").asText());
+    transcript.put("name", node.path("name").asText());
+    transcript.put("is_canonical", node.path("isCanonical").asBoolean());
+    transcript.put("length", node.path("length").asLong());
+    transcript.put("length_amino_acid", node.path("lengthAminoAcid").asLong());
+    transcript.put("length_dna", node.path("lengthDNA").asLong());
+    transcript.put("number_of_exons", node.path("numberOfExons").asLong());
+    transcript.put("start_exon", node.path("startExon").asLong());
+    transcript.put("seq_exon_start", node.path("seqExonStart").asLong());
+    transcript.put("seq_exon_end", node.path("seqExonEnd").asLong());
+    transcript.put("end_exon", node.path("endExon").asLong());
+    transcript.put("translation_id", node.path("translationId").asText());
 
     // Collections
-    transcript.put("exons", record.path("exons"));
-    transcript.put("domains", domains(record));
+    transcript.put("exons", node.path("exons"));
+    transcript.put("domains", domains(node));
 
     return transcript;
   }
 
-  private JsonNode domains(JsonNode record) {
-    JsonNode domains = record.path("domains");
+  private JsonNode domains(JsonNode node) {
+    JsonNode values = node.path("domains");
 
-    ArrayNode arrayNode = mapper.createArrayNode();
-    for(JsonNode domain : domains) {
-      final String gffSource = domain.path("gffSource").asText();
+    ArrayNode domains = mapper.createArrayNode();
+    for(JsonNode value : values) {
+      final String gffSource = value.path("gffSource").asText();
 
       // Only add "Pfam" sources - Junjun
       final boolean pFam = "Pfam".equals(gffSource);
       if(pFam) {
-        arrayNode.add(domain);
+        domains.add(domain(value));
       }
     }
 
-    return arrayNode;
+    return domains;
+  }
+
+  private JsonNode domain(JsonNode node) {
+    ObjectNode domain = mapper.createObjectNode();
+
+    // Simple
+    domain.put("interpro_id", node.path("interproId").asText());
+    domain.put("hit_name", node.path("hitName").asText());
+    domain.put("gff_source", node.path("gffSource").asText());
+    domain.put("description", node.path("description").asText());
+    domain.put("start", node.path("start").asLong());
+    domain.put("end", node.path("end").asLong());
+
+    return domain;
   }
 
 }
