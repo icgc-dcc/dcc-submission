381c231f9b772f8b31ffae7087f16c71e0f1b2b0
Anthony Cros <anthony.cros@oicr.on.ca>
2013-05-31 15:20:31 -0400

    ulimit-script - fixes
    Former-commit-id: da4ed233d3b8e7183d5813debc15c0a3c8d33924
    Former-commit-id: 081879c0b02a6c0f7acd533598d4f5d3d33acd70

--- src/main/scripts/dcc.sh
+++ src/main/scripts/dcc.sh
@@ -30,41 +30,47 @@ function process() {
  host=${1?} && shift
  user=${1?} && shift
  keyword=${1?} && shift
- ssh ${host?} 'bash -s' < ${script?} ${host?} ${user?} "${keyword?}"
+
+ tmp_script="./tmp/sh"
+ echo "${script?} \"${host?}\" \"${user?}\" \"${keyword?}\"" > ${tmp_script?} # somehow can't seem to pass parameters to sudo otherwise
+ chmod +x ${tmp_script?}
+
+ ssh ${host?} 'sudo -u hdfs -s' < ${tmp_script?}
+ rm ${tmp_script?}
 }
 
 # ===========================================================================
 # explore each host and report on the effective max_open_files value
 
 ## www: hdfs     30367 23.6 25.0 2530496 2028996 pts/3 Sl   May30 258:30 java -Dlogback.configurationFile=/var/lib/hdfs/dcc/server/logback.xml -cp dcc-submission-server-1.6-SNAPSHOT.jar org.icgc.dcc.Main external /var/lib/hdfs/application.conf
-process ${www?} "hdfs" "dcc.Main"
+process "${www?}" "hdfs" "dcc.Main"
 
 # ---------------------------------------------------------------------------
 
 ## nn: hdfs      1729  0.1  0.3 1328816 246636 ?      Sl   Apr24  83:36 /usr/lib/jvm/j2sdk1.6-oracle/jre/bin/java -Dproc_namenode -Xmx1000m -Dhdfs.audit.logger=INFO,RFAAUDIT -Dsecurity.audit.logger=INFO,RFAS -Djava.net.preferIPv4Stack=true -Dhadoop.log.dir=/var/log/hadoop-hdfs -Dhadoop.log.file=hadoop-cmf-hdfs1-NAMENODE-hcn50.res.oicr.on.ca.log.out -Dhadoop.home.dir=/usr/lib/hadoop -Dhadoop.id.str=hdfs -Dhadoop.root.logger=INFO,RFA -Djava.library.path=/usr/lib/hadoop/lib/native -Dhadoop.policy.file=hadoop-policy.xml -Djava.net.preferIPv4Stack=true -Xmx720732160 -Dhadoop.security.logger=INFO,RFAS org.apache.hadoop.hdfs.server.namenode.NameNode
-process ${nn?} "hdfs" "namenode"
+process "${nn?}" "hdfs" "namenode"
 
 # ---------------------------------------------------------------------------
 
 ## nn2: ?
 if [ -n "${nn2?}" ]; then
- process ${nn2?} "hdfs" "secondary" # FIXME: not sure about the keyword...
+ process "${nn2?}" "hdfs" "secondary" # FIXME: not sure about the keyword...
 fi
 
 # ---------------------------------------------------------------------------
 
 ## jt: mapred    1989  0.6  1.4 3346372 942132 ?      Sl   Apr22 372:05 /usr/lib/jvm/j2sdk1.6-oracle/jre/bin/java -Dproc_jobtracker -Xmx1000m -Djava.net.preferIPv4Stack=true -Xmx2755907842 -Dhadoop.event.appender=,EventCatcher -Dhadoop.log.dir=/var/log/hadoop-0.20-mapreduce -Dhadoop.log.file=hadoop-cmf-mapreduce1-JOBTRACKER-hcn51.res.oicr.on.ca.log.out -Dhadoop.home.dir=/usr/lib/hadoop-0.20-mapreduce -Dhadoop.id.str= -Dhadoop.root.logger=INFO,RFA -Djava.library.path=/usr/lib/hadoop-0.20-mapreduce/lib/native/Linux-amd64-64 -Dhadoop.policy.file=hadoop-policy.xml -classpath /var/run/cloudera-scm-agent/process/305-mapreduce-JOBTRACKER:/usr/lib/jvm/j2sdk1.6-oracle/jre/lib/tools.jar:/usr/lib/hadoop-0.20-mapreduce:/usr/lib/hadoop-0.20-mapreduce/hadoop-core-2.0.0-mr1-cdh4.1.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/activation-1.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/ant-contrib-1.0b3.jar:/usr/lib/hadoop-0.20-mapreduce/lib/asm-3.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/aspectjrt-1.6.5.jar:/usr/lib/hadoop-0.20-mapreduce/lib/aspectjtools-1.6.5.jar:/usr/lib/hadoop-0.20-mapreduce/lib/avro-1.7.1.cloudera.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/avro-compiler-1.7.1.cloudera.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-beanutils-1.7.0.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-beanutils-core-1.8.0.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-cli-1.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-codec-1.4.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-collections-3.2.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-configuration-1.6.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-digester-1.8.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-el-1.0.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-httpclient-3.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-io-2.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-lang-2.5.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-logging-1.1.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-math-2.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/commons-net-3.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/guava-11.0.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/hadoop-fairscheduler-2.0.0-mr1-cdh4.1.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/hsqldb-1.8.0.10.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jackson-core-asl-1.8.8.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jackson-jaxrs-1.8.8.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jackson-mapper-asl-1.8.8.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jackson-xc-1.8.8.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jasper-compiler-5.5.23.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jasper-runtime-5.5.23.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jaxb-api-2.2.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jaxb-impl-2.2.3-1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jersey-core-1.8.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jersey-json-1.8.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jersey-server-1.8.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jets3t-0.6.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jettison-1.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jetty-6.1.26.cloudera.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jetty-util-6.1.26.cloudera.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jsch-0.1.42.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jsp-api-2.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jsr305-1.3.9.jar:/usr/lib/hadoop-0.20-mapreduce/lib/junit-4.8.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/kfs-0.2.2.jar:/usr/lib/hadoop-0.20-mapreduce/lib/kfs-0.3.jar:/usr/lib/hadoop-0.20-mapreduce/lib/log4j-1.2.17.jar:/usr/lib/hadoop-0.20-mapreduce/lib/mockito-all-1.8.5.jar:/usr/lib/hadoop-0.20-mapreduce/lib/paranamer-2.3.jar:/usr/lib/hadoop-0.20-mapreduce/lib/protobuf-java-2.4.0a.jar:/usr/lib/hadoop-0.20-mapreduce/lib/servlet-api-2.5.jar:/usr/lib/hadoop-0.20-mapreduce/lib/slf4j-api-1.6.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/snappy-java-1.0.4.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/stax-api-1.0.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/xmlenc-0.52.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jsp-2.1/jsp-2.1.jar:/usr/lib/hadoop-0.20-mapreduce/lib/jsp-2.1/jsp-api-2.1.jar:/usr/lib/hadoop-0.20-mapreduce/contrib/capacity-scheduler/hadoop-capacity-scheduler-2.0.0-mr1-cdh4.1.2.jar:/usr/share/cmf/lib/plugins/event-publish-4.1.2-shaded.jar:/usr/lib/hado
-process ${jt?} "mapred" "jobtracker"
+process "${jt?}" "mapred" "jobtracker"
 
 # ---------------------------------------------------------------------------
 
 ## dn/tt:
 for worker in ${workers?}; do
  # dn: hdfs      1680  0.5  1.7 2302768 1113320 ?     Sl   Mar22 601:08 /usr/lib/jvm/j2sdk1.6-oracle/jre/bin/java -Dproc_datanode -Xmx1000m -Dhdfs.audit.logger=INFO,RFAAUDIT -Dsecurity.audit.logger=INFO,RFAS -Djava.net.preferIPv4Stack=true -Dhadoop.log.dir=/var/log/hadoop-hdfs
- process ${worker?} "hdfs" "datanode"
+ process "${worker?}" "hdfs" "datanode"
 
  # tt: mapred    1875  0.5  0.6 2810332 421824 ?      Sl   Mar22 544:27 /usr/lib/jvm/j2sdk1.6-oracle/jre/bin/java -Dproc_tasktracker -Xmx1000m -Djava.net.preferIPv4Stack=true -Xmx864878592 -Dhadoop.event.appender=,EventCatcher -Dhadoop.log.dir=/var/log/hadoop-0.20-mapreduce -D
- process ${worker?} "mapred" "tasktracker"
+ process "${worker?}" "mapred" "tasktracker"
 done
 
 # ---------------------------------------------------------------------------
--- src/main/scripts/limit.sh
+++ src/main/scripts/limit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 # usage: use via dcc.sh only
 # prints the max_open_files value for a given host/user/PID based on the given keyword (to infer the PID)
 
@@ -7,14 +7,11 @@
 host=${1?} && shift
 user=${1?} && shift
 keyword=${1?} && shift
-echo -ne "${host?}\t${user?}\t${keyword?}" # report arguments
-
-# change to user of interest
-sudo -u ${user?} -i
+echo -ne "\"${host?}\"\t\"${user?}\"\t\"${keyword?}\"" # report arguments
 
 # get PID corresponding to the process matching the keyword provided (typically "tasktracker", "mongod", ...); there should be only 1 match
-[ 1 == "$(ps aux | grep \"${keyword?}\" | grep -v grep | wc -l)" ] || { echo -e "\nERROR: more than one matching PID found."; exit 1; }
-pid=$(ps aux | grep "${keyword?}" | grep -v grep | awk '{print $2}')
+[ 1 == "$(ps aux | grep \"${keyword?}\" | grep -v bash | grep -v grep | wc -l)" ] || { echo -e "\nERROR: not exactly one matching PID found: $(ps aux | grep \"${keyword?}\")"; exit 1; }
+pid=$(ps aux | grep "${keyword?}" | grep -v bash | grep -v grep | awk '{print $2}')
 
 if [ -n "$pid" ]; then # if a pid was found, get the value for max_open_files
  max_open_files=$(cat /proc/${pid?}/limits | grep open | awk '{print $4}')
@@ -22,10 +19,9 @@ else # else print something obvious like "?"
  pid="?"
  max_open_files="?"
 fi
-echo -ne "\t$pid\t${max_open_files?}" # report values
+echo -ne "\t\"${pid?}\"\t\"${max_open_files?}\"" # report values
 
 # ===========================================================================
 
 echo
-exit
 
