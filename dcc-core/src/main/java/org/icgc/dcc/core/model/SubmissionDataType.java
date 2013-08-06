79f8a21681351dc03a2514a6b8f0ade90a4f20e9
Anthony Cros <anthony.cros@oicr.on.ca>
2013-08-06 13:41:07 -0400

    DCC-1240--add-loader-counts - moved meta/primary/secondary/gene to an enum as well (phase 2)
    Former-commit-id: 307e06db288896f58c961b86131f3b88535a04fa
    Former-commit-id: 16c205964ee07d15b98e4aeb0a9cd2b92e7a315b

---
+++ dcc-core/src/main/java/org/icgc/dcc/core/model/SubmissionDataType.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2013 The Ontario Institute for Cancer Research. All rights reserved.                             
+ *                                                                                                               
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with                                  
+ * this program. If not, see <http://www.gnu.org/licenses/>.                                                     
+ *                                                                                                               
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.icgc.dcc.core.model;
+
+/**
+ * Represents an ICGC data type, such as "donor", "specimen", "ssm", "meth", ...
+ * <p>
+ * Careful not to confuse this with {@link SubmissionFileType} which represents the ICGC file types, such as "donor",
+ * "specimen", "ssm_m", "meth_m", ... They have the clinical ones in common.
+ */
+public interface SubmissionDataType {
+
+  String TYPE_SUFFIX = "_TYPE";
+
+  String getTypeName();
+
+}
