87ab84252ca3ca926b6702aa5aa96e772efc7fd2
Bob Tiernay <btiernay@oicr.on.ca>
2013-10-26 17:54:11 -0400

    Factored dcc-submission-server into dcc-submission-validator module.
    Former-commit-id: d660371df1a7df28fdbf5919146f06a96336cdb4
    Former-commit-id: ef105356bfa22b968b42fc9910704f42ce2982aa

---
+++ dcc-submission/dcc-submission-validator/README.md
@@ -0,0 +1,13 @@
+ICGC DCC - Submission Validator
+===
+
+Validator library module for the data submission sub-system. 
+
+Build
+---
+
+	mvn
+
+	
+
+
--- dcc-submission/dcc-submission-validator/src/test/java/org/icgc/dcc/submission/validation/ValidationInternalIntegrityTest.java
+++ dcc-submission/dcc-submission-validator/src/test/java/org/icgc/dcc/submission/validation/ValidationInternalIntegrityTest.java
@@ -21,25 +21,20 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 import static org.apache.commons.io.FileUtils.readFileToString;
-import static org.icgc.dcc.submission.TestUtils.dictionary;
-import static org.icgc.dcc.submission.TestUtils.resourceToString;
 import static org.icgc.dcc.submission.validation.CascadingStrategy.SEPARATOR;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import lombok.SneakyThrows;
 import lombok.val;
 
 import org.apache.hadoop.fs.Path;
 import org.codehaus.jackson.JsonProcessingException;
-import org.icgc.dcc.submission.dictionary.DictionaryService;
 import org.icgc.dcc.submission.dictionary.model.CodeList;
 import org.icgc.dcc.submission.dictionary.model.Dictionary;
 import org.icgc.dcc.submission.dictionary.model.Field;
@@ -47,13 +42,8 @@ import org.icgc.dcc.submission.dictionary.model.FileSchema;
 import org.icgc.dcc.submission.dictionary.model.Restriction;
 import org.icgc.dcc.submission.dictionary.model.RestrictionType;
 import org.icgc.dcc.submission.dictionary.model.Term;
-import org.icgc.dcc.submission.fs.DccFileSystem;
-import org.icgc.dcc.submission.fs.GuiceJUnitRunner;
-import org.icgc.dcc.submission.fs.GuiceJUnitRunner.GuiceModules;
-import org.icgc.dcc.submission.fs.SubmissionDirectory;
 import org.icgc.dcc.submission.release.model.QueuedProject;
 import org.icgc.dcc.submission.validation.cascading.ForbiddenValuesFunction;
-import org.icgc.dcc.submission.validation.factory.LocalCascadingStrategyFactory;
 import org.icgc.dcc.submission.validation.restriction.CodeListRestriction;
 import org.icgc.dcc.submission.validation.restriction.DiscreteValuesRestriction;
 import org.icgc.dcc.submission.validation.restriction.RangeFieldRestriction;
@@ -65,69 +55,39 @@ import org.icgc.dcc.submission.validation.visitor.UniqueFieldsPlanningVisitor;
 import org.icgc.dcc.submission.validation.visitor.ValueTypePlanningVisitor;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import com.google.common.base.Optional;
-import com.google.inject.Inject;
 import com.mongodb.BasicDBObject;
 
-@RunWith(GuiceJUnitRunner.class)
-@GuiceModules({ ValidationTestModule.class })
-public class ValidationInternalIntegrityTest {
+public class ValidationInternalIntegrityTest extends BaseValidationIntegrityTest {
 
   /**
    * Test data.
    */
   private static final String ROOT_DIR = "/fixtures/validation/internal";
-  private static final QueuedProject QUEUED_PROJECT = new QueuedProject("dummyProject", null); // TODO: mock
-
-  /**
-   * Dependencies.
-   */
-  @Inject
-  private DictionaryService dictionaryService;
-  @Inject
-  private Planner planner;
-
-  private ValidationService validationService;
-  private SubmissionDirectory submissionDirectory;
-  private Dictionary dictionary;
+  private static final QueuedProject QUEUED_PROJECT = new QueuedProject("dummyProject", null);
 
   @Before
   public void setUp() throws JsonProcessingException, IOException {
-    DccFileSystem dccFileSystem = mock(DccFileSystem.class);
-
-    CodeList codeList1 = mock(CodeList.class);
-    CodeList codeList2 = mock(CodeList.class);
-    CodeList codeList3 = mock(CodeList.class);
-    CodeList codeList4 = mock(CodeList.class);
-
-    submissionDirectory = mock(SubmissionDirectory.class);
-
-    List<Term> termList1 = Arrays.asList(new Term("1", "dummy", null), new Term("2", "dummy", null));
-    List<Term> termList2 = Arrays.asList(new Term("1", "dummy", null), new Term("2", "dummy", null));
-    List<Term> termList3 =
+    val termList1 = Arrays.asList(new Term("1", "dummy", null), new Term("2", "dummy", null));
+    val termList2 = Arrays.asList(new Term("1", "dummy", null), new Term("2", "dummy", null));
+    val termList3 =
         Arrays.asList(new Term("1", "dummy", null), new Term("2", "dummy", null), new Term("3", "dummy", null),
             new Term("4", "dummy", null), new Term("5", "dummy", null));
-    List<Term> termList4 =
+    val termList4 =
         Arrays.asList(new Term("1", "dummy", null), new Term("2", "dummy", null), new Term("3", "dummy", null));
 
-    when(dictionaryService.getCodeList(anyString())).thenReturn(Optional.<CodeList> absent());
+    when(context.getCodeList(anyString())).thenReturn(Optional.<CodeList> absent());
 
-    when(dictionaryService.getCodeList("donor.0.donor_sex.v1")).thenReturn(Optional.of(codeList1));
-    when(dictionaryService.getCodeList("donor.0.donor_vital_status.v1")).thenReturn(Optional.of(codeList2));
-    when(dictionaryService.getCodeList("donor.0.disease_status_last_followup.v1")).thenReturn(Optional.of(codeList3));
-    when(dictionaryService.getCodeList("donor.0.donor_relapse_type.v1")).thenReturn(Optional.of(codeList4));
+    when(context.getCodeList("donor.0.donor_sex.v1")).thenReturn(Optional.of(codeList1));
+    when(context.getCodeList("donor.0.donor_vital_status.v1")).thenReturn(Optional.of(codeList2));
+    when(context.getCodeList("donor.0.disease_status_last_followup.v1")).thenReturn(Optional.of(codeList3));
+    when(context.getCodeList("donor.0.donor_relapse_type.v1")).thenReturn(Optional.of(codeList4));
 
     when(codeList1.getTerms()).thenReturn(termList1);
     when(codeList2.getTerms()).thenReturn(termList2);
     when(codeList3.getTerms()).thenReturn(termList3);
     when(codeList4.getTerms()).thenReturn(termList4);
-
-    validationService =
-        new ValidationService(planner, dccFileSystem, dictionaryService, new LocalCascadingStrategyFactory());
-
-    dictionary = dictionary();
   }
 
   @Test
@@ -237,7 +197,7 @@ public class ValidationInternalIntegrityTest {
     val submissionFilePath = "/fixtures/validation/internal/error/" + errorType;
     val content = validate(validationService, dictionary, submissionFilePath);
 
-    val expected = resourceToString("/fixtures/validation/reference/" + errorType + ".json");
+    val expected = getResource("/fixtures/validation/reference/" + errorType + ".json");
     assertEquals("errorType = " + errorType + ", content = " + content, expected.trim(), content.trim());
   }
 
@@ -253,7 +213,7 @@ public class ValidationInternalIntegrityTest {
 
     Path rootDir = new Path(rootDirString);
     Path outputDir = new Path(outputDirString);
-    Path systemDir = new Path("src/test/resources/fixtures/submission/fs/SystemFiles");
+    Path systemDir = SYSTEM_DIR;
 
     CascadingStrategy cascadingStrategy = new LocalCascadingStrategy(rootDir, outputDir, systemDir);
 
@@ -265,26 +225,6 @@ public class ValidationInternalIntegrityTest {
 
     assertTrue(errorFileString, errorFile.exists());
     return readFileToString(errorFile);
-  }
-
-  private static FileSchema getFileSchemaByName(Dictionary dictionary, String name) {
-    for (val fileSchema : dictionary.getFiles()) {
-      if (name.equals(fileSchema.getName())) {
-        return fileSchema;
-      }
-    }
-
-    return null;
-  }
-
-  private static Field getFieldByName(FileSchema fileSchema, String name) {
-    for (val field : fileSchema.getFields()) {
-      if (name.equals(field.getName())) {
-        return field;
-      }
-    }
-
-    return null;
   }
 
 }
