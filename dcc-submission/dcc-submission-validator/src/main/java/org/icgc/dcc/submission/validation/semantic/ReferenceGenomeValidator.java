43b54af655daa48354dd3fbce8278229ff5f910a
Bob Tiernay <btiernay@oicr.on.ca>
2013-11-07 11:31:58 -0500

    Added chromosome code to value conversion for FASTA lookup.
    Former-commit-id: e6c8757db1e4fee7401a3a1becf08a1c5397cd88
    Former-commit-id: 5161cd1212a4ca1b425ddc8de118eb47063f44cd

--- dcc-submission/dcc-submission-validator/src/main/java/org/icgc/dcc/submission/validation/semantic/ReferenceGenomeValidator.java
+++ dcc-submission/dcc-submission-validator/src/main/java/org/icgc/dcc/submission/validation/semantic/ReferenceGenomeValidator.java
@@ -19,10 +19,15 @@ package org.icgc.dcc.submission.validation.semantic;
 
 import static com.google.common.io.ByteStreams.copy;
 import static com.google.common.io.Files.getFileExtension;
+import static com.google.common.primitives.Ints.tryParse;
 import static java.util.Arrays.asList;
+import static org.icgc.dcc.core.model.FieldNames.SubmissionFieldNames.SUBMISSION_OBSERVATION_CHROMOSOME;
+import static org.icgc.dcc.core.model.FieldNames.SubmissionFieldNames.SUBMISSION_OBSERVATION_CHROMOSOME_END;
+import static org.icgc.dcc.core.model.FieldNames.SubmissionFieldNames.SUBMISSION_OBSERVATION_CHROMOSOME_START;
 import static org.icgc.dcc.core.model.FieldNames.SubmissionFieldNames.SUBMISSION_OBSERVATION_REFERENCE_GENOME_ALLELE;
 import static org.icgc.dcc.submission.validation.core.ErrorType.REFERENCE_GENOME_ERROR;
 import static org.icgc.dcc.submission.validation.core.Validators.checkState;
+import static org.icgc.dcc.submission.validation.platform.PlatformStrategy.FIELD_SEPARATOR;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -44,8 +49,6 @@ import net.sf.picard.reference.IndexedFastaSequenceFile;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.compress.BZip2Codec;
-import org.icgc.dcc.core.model.FieldNames.SubmissionFieldNames;
-import org.icgc.dcc.submission.validation.core.ReportContext;
 import org.icgc.dcc.submission.validation.core.ValidationContext;
 import org.icgc.dcc.submission.validation.core.Validator;
 
@@ -62,16 +65,24 @@ import com.google.common.io.LineReader;
 @NoArgsConstructor
 public class ReferenceGenomeValidator implements Validator {
 
-  public static final String REFERENCE_GENOME_VERSION = "GrCh37";
+  /**
+   * The GRC h37 (patch 0) standard ICGC DCC version.
+   */
+  public static final String REFERENCE_GENOME_VERSION = "GrCh37" /* .0 */;
+
+  /**
+   * Remote FASTA location
+   */
   public static final String REFERENCE_GENOME_BASE_URL = "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference";
   public static final String REFERENCE_GENOME_DATA_URL = REFERENCE_GENOME_BASE_URL + "/" + "human_g1k_v37.fasta.gz";
   public static final String REFERENCE_GENOME_INDEX_URL = REFERENCE_GENOME_BASE_URL + "/" + "human_g1k_v37.fasta.fai";
 
+  /**
+   * Local FASTA location
+   */
   public static final String LOCAL_DIR = "/tmp";
   public static final String LOCAL_DATA_FILE = LOCAL_DIR + "/" + "referenceGenome.fasta";
   public static final String LOCAL_INDEX_FILE = LOCAL_DIR + "/" + "referenceGenome.fasta.fai";
-
-  private static final String FIELD_SEPARATOR = "\t";
 
   private IndexedFastaSequenceFile sequenceFile;
 
@@ -86,16 +97,18 @@ public class ReferenceGenomeValidator implements Validator {
    */
   @SneakyThrows
   public void ensureDownload() {
-    log.info("Checking if '{}' and '{}' exist", LOCAL_DATA_FILE, LOCAL_INDEX_FILE);
+    log.info("Checking if FASTA '{}' and '{}' files exist", LOCAL_DATA_FILE, LOCAL_INDEX_FILE);
     val downloaded = new File(LOCAL_DATA_FILE).exists() && new File(LOCAL_INDEX_FILE).exists();
     if (downloaded) {
-      log.info("Files exist");
+      // Already available
+      log.info("FASTA files exist");
       sequenceFile = new IndexedFastaSequenceFile(new File(LOCAL_DATA_FILE));
 
       return;
     }
 
-    log.warn("Missing '{}' and / or '{}'", LOCAL_INDEX_FILE, LOCAL_DATA_FILE);
+    // Needs downloading. Ensure correct permissions in deployments!
+    log.warn("Missing  FASTA '{}' and / or '{}' files(s)", LOCAL_INDEX_FILE, LOCAL_DATA_FILE);
     download(REFERENCE_GENOME_INDEX_URL, LOCAL_INDEX_FILE, false);
     download(REFERENCE_GENOME_DATA_URL, LOCAL_DATA_FILE, true);
 
@@ -115,54 +128,36 @@ public class ReferenceGenomeValidator implements Validator {
 
     // This validation is only applicable if ssm_p is available
     Optional<Path> optionalSsmPrimaryFile = context.getSsmPrimaryFile();
-    if (!optionalSsmPrimaryFile.isPresent()) {
+    val skip = !optionalSsmPrimaryFile.isPresent();
+    if (skip) {
       log.info("No ssm_p file for '{}'", context.getProjectKey());
+
+      return;
     }
 
-    // Create a streaming reader
+    // Exists
     val ssmPrimaryFile = optionalSsmPrimaryFile.get();
-    val reader = getLineReader(ssmPrimaryFile, context.getFileSystem());
+
+    // Source input
+    @Cleanup
+    val inputStream = getInputStream(ssmPrimaryFile, context.getFileSystem());
 
     // Get to work
     log.info("Performing reference genome validation on file '{}' for '{}'", ssmPrimaryFile, context.getProjectKey());
-    validate(context, ssmPrimaryFile.getName(), reader);
+    validate(context, ssmPrimaryFile.getName(), inputStream);
     log.info("Finished performing reference genome validation for '{}'", context.getProjectKey());
-    log.info("Finished");
   }
 
-  private void validate(ReportContext context, String fileName, LineReader reader) throws IOException {
-    // Field indexes
-    int chromosomeIdx = -1;
-    int startIdx = -1;
-    int endIdx = -1;
-    int referenceAlleleIdx = -1;
+  private void validate(final ValidationContext context, final String fileName, InputStream inputStream) {
+    val reader = new LineReader(new InputStreamReader(inputStream));
+    parse(reader, new LineProcessor() {
 
-    // Line state (one-based)
-    long lineNumber = 1;
-    String line;
-
-    // Read all lines
-    while ((line = reader.readLine()) != null) {
-      val fields = parseLine(line);
-
-      if (lineNumber == 1) {
-        // Get column position
-        chromosomeIdx = fields.indexOf(SubmissionFieldNames.SUBMISSION_OBSERVATION_CHROMOSOME);
-        startIdx = fields.indexOf(SubmissionFieldNames.SUBMISSION_OBSERVATION_CHROMOSOME_START);
-        endIdx = fields.indexOf(SubmissionFieldNames.SUBMISSION_OBSERVATION_CHROMOSOME_END);
-        referenceAlleleIdx = fields.indexOf(SubmissionFieldNames.SUBMISSION_OBSERVATION_REFERENCE_GENOME_ALLELE);
-      } else {
-        val start = fields.get(startIdx);
-        val end = fields.get(endIdx);
-
-        // FIXME: This needs to be converted from CodeList "code" to "value", which means this class needs at least the
-        // CodeList to be injected, by using the context
-        val chromosome = fields.get(chromosomeIdx);
-
-        val referenceAllele = fields.get(referenceAlleleIdx);
+      @Override
+      public void process(long lineNumber, String start, String end, String chromosomeCode, String referenceAllele) {
+        val chromosome = getChromosome(context, chromosomeCode);
         val referenceSequence = getReferenceGenomeSequence(chromosome, start, end);
 
-        boolean mismatch = !isMatch(referenceAllele, referenceSequence);
+        val mismatch = !isMatch(referenceAllele, referenceSequence);
         if (mismatch) {
           context.reportError(
               fileName,
@@ -174,14 +169,45 @@ public class ReferenceGenomeValidator implements Validator {
               // Params
               REFERENCE_GENOME_VERSION);
         }
+
+        // Cooperate
+        checkState(getName());
       }
 
-      // Cooperate
-      checkState(getName());
+    });
+  }
+
+  @SneakyThrows
+  public void parse(LineReader reader, LineProcessor callback) {
+    // @formatter:off
+    // Field indexes
+    int      chromosomeIdx = -1;
+    int           startIdx = -1;
+    int             endIdx = -1;
+    int referenceAlleleIdx = -1;
+
+    // Line state (one-based)
+    long lineNumber = 1;
+    String line;
+
+    // Read all lines
+    while ((line = reader.readLine()) != null) {
+      val fields = parseLine(line);
+
+      if (lineNumber == 1) {
+              chromosomeIdx = fields.indexOf(SUBMISSION_OBSERVATION_CHROMOSOME);
+                   startIdx = fields.indexOf(SUBMISSION_OBSERVATION_CHROMOSOME_START);
+                     endIdx = fields.indexOf(SUBMISSION_OBSERVATION_CHROMOSOME_END);
+         referenceAlleleIdx = fields.indexOf(SUBMISSION_OBSERVATION_REFERENCE_GENOME_ALLELE);
+      } else {
+        // Delegate logic
+        callback.process(lineNumber, fields.get(startIdx), fields.get(endIdx), fields.get(chromosomeIdx), fields.get(referenceAlleleIdx));
+      }
 
       // Book-keeping
       lineNumber++;
     }
+    // @formatter:on
   }
 
   public String getReferenceGenomeSequence(String chromosome, String start, String end) {
@@ -217,6 +243,33 @@ public class ReferenceGenomeValidator implements Validator {
     return compressed ? new GZIPInputStream(inputStream) : inputStream;
   }
 
+  private static String getChromosome(ValidationContext context, String chromosomeCode) {
+    val value = tryParse(chromosomeCode);
+
+    val alpha = value == null;
+    if (alpha) {
+      // CodeList value was passed
+      return chromosomeCode;
+    }
+
+    if (value >= 1 && value <= 22) {
+      // CodeList value and terms are the same
+      return chromosomeCode;
+    }
+
+    if (value == 23) {
+      return "X";
+    }
+    if (value == 24) {
+      return "Y";
+    }
+    if (value == 25) {
+      return "MT";
+    }
+
+    throw new IllegalStateException("Could not convert term for code '" + chromosomeCode + "'");
+  }
+
   private static List<String> parseLine(String line) {
     return asList(line.split(FIELD_SEPARATOR));
   }
@@ -230,21 +283,29 @@ public class ReferenceGenomeValidator implements Validator {
   }
 
   /**
-   * Returns a LineReader capable of reading gz, bzip2 or plain text files
+   * Returns a {@InputStream} capable of reading {@code gz}, {@code bzip2} or plain text files
    */
-  private static LineReader getLineReader(Path path, FileSystem fileSystem) throws IOException {
+  private static InputStream getInputStream(Path path, FileSystem fileSystem) throws IOException {
     val extension = getFileExtension(path.getName());
     val gzip = extension.equals("gz");
     val bzip2 = extension.equals("bz2");
 
     // @formatter:off
     val inputStream = fileSystem.open(path);
-    return new LineReader(new InputStreamReader(
+    return 
         gzip  ? new GZIPInputStream(inputStream)                : 
         bzip2 ? new BZip2Codec().createInputStream(inputStream) :
-                inputStream
-        ));
+                inputStream;
     // @formatter:on
+  }
+
+  /**
+   * Simple callback for line processing.
+   */
+  private interface LineProcessor {
+
+    void process(long lineNumber, String start, String end, String chromosome, String referenceAllele);
+
   }
 
 }
