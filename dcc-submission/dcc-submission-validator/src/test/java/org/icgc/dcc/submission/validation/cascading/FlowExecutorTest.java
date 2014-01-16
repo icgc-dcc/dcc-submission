060e1e8727151be0bef72ff5c4f72960197c79df
Bob Tiernay <btiernay@oicr.on.ca>
2014-01-16 17:08:06 -0500

    Added tests for exceptional conditions in both local and hadoop FlowExecutor usages.
    Former-commit-id: 07db1d5b4a18ca3d478b89bd213542e967add51b
    Former-commit-id: 9b6adbf8244f7942c64dc0fb858f7d217d7bc0da

--- dcc-submission/dcc-submission-validator/src/test/java/org/icgc/dcc/submission/validation/cascading/FlowExecutorTest.java
+++ dcc-submission/dcc-submission-validator/src/test/java/org/icgc/dcc/submission/validation/cascading/FlowExecutorTest.java
@@ -11,13 +11,34 @@ import org.apache.hadoop.conf.Configuration;
 import org.icgc.dcc.submission.validation.MiniHadoop;
 import org.junit.Test;
 
+import cascading.flow.FlowException;
+
 import com.google.common.collect.ImmutableMap;
 
 @Slf4j
 public class FlowExecutorTest {
 
   @Test
-  public void testExecute() throws IOException {
+  public void testExecuteHadoopSuccess() throws IOException {
+    executeHadoop(new SuccessTask());
+  }
+
+  @Test(expected = FlowException.class)
+  public void testExecuteHadoopFailure() throws IOException {
+    executeHadoop(new FailureTask());
+  }
+
+  @Test
+  public void testExecuteLocalSuccess() throws IOException {
+    executeLocal(new SuccessTask());
+  }
+
+  @Test(expected = FlowException.class)
+  public void testExecuteLocalFailure() throws IOException {
+    executeLocal(new FailureTask());
+  }
+
+  private static void executeHadoop(Runnable runnable) throws IOException {
     val hadoop = createHadoop();
     val jobConf = hadoop.createJobConf();
     val properties = ImmutableMap.<Object, Object> of(
@@ -26,19 +47,34 @@ public class FlowExecutorTest {
         );
 
     val executor = new FlowExecutor(properties);
-    executor.execute(new Task());
-
+    executor.execute(runnable);
   }
 
-  private MiniHadoop createHadoop() throws IOException {
+  private static void executeLocal(Runnable runnable) throws IOException {
+    val properties = ImmutableMap.<Object, Object> of();
+
+    val executor = new FlowExecutor(properties);
+    executor.execute(runnable);
+  }
+
+  private static MiniHadoop createHadoop() throws IOException {
     return new MiniHadoop(new Configuration(), 1, 1, new File("/tmp/hadoop"));
   }
 
-  private static class Task implements Runnable, Serializable {
+  private static class SuccessTask implements Runnable, Serializable {
 
     @Override
     public void run() {
       log.info("Running!");
+    }
+
+  }
+
+  private static class FailureTask implements Runnable, Serializable {
+
+    @Override
+    public void run() {
+      throw new RuntimeException();
     }
 
   }
