6412db3bb4eee6d2e8bd7d41e49a3606ccdb2d93
Bob Tiernay <btiernay@oicr.on.ca>
2014-01-28 21:34:34 -0500

    Updated dictionary to 0.7e. Updated heliotrope to latest dump.
    Former-commit-id: 3d9a5cf70eefd4c52dfce62a01a3ebeb0e2514cd
    Former-commit-id: dc9fb13aaa76d7fb87e8ffec6e5f94c538217380

--- src/main/scripts/DCC-1885/test_data.sh
+++ src/main/scripts/DCC-1885/test_data.sh
@@ -16,42 +16,42 @@ git checkout -- ../../../../dcc-submission/dcc-submission-server/src/test/resour
 rm -rf /tmp/DCC-1885 || :
 mkdir /tmp/DCC-1885
 
-curl -v -XGET -H "Accept: application/json" http://***REMOVED***:5380/ws/nextRelease/dictionary | json_pp > /tmp/DCC-1885/0.7a.json # TODO: use resources
+curl -v -XGET -H "Accept: application/json" http://***REMOVED***:5380/ws/nextRelease/dictionary | json_pp > /tmp/DCC-1885/0.7e.json # TODO: use resources
 printf '=%.0s' {1..75}
 
 ./migration.sh \
  ../../../../dcc-submission/dcc-submission-server/src/test/resources/fixtures/submission/dcc_root_dir/release1/project.1 \
- /tmp/DCC-1885/0.7a.json \
+ /tmp/DCC-1885/0.7e.json \
  /tmp/DCC-1885/project.1
 
 ./migration.sh \
  ../../../../dcc-submission/dcc-submission-server/src/test/resources/fixtures/submission/dcc_root_dir/release1/project.2 \
- /tmp/DCC-1885/0.7a.json \
+ /tmp/DCC-1885/0.7e.json \
  /tmp/DCC-1885/project.2
 
 ./migration.sh \
  ../../../../dcc-submission/dcc-submission-server/src/test/resources/fixtures/submission/dcc_root_dir/release1/project.3 \
- /tmp/DCC-1885/0.7a.json \
+ /tmp/DCC-1885/0.7e.json \
  /tmp/DCC-1885/project.3
 
 ./migration.sh \
  ../../../../dcc-submission/dcc-submission-server/src/test/resources/fixtures/submission/dcc_root_dir/release1/project.4 \
- /tmp/DCC-1885/0.7a.json \
+ /tmp/DCC-1885/0.7e.json \
  /tmp/DCC-1885/project.4
 
 ./migration.sh \
  ../../../../dcc-submission/dcc-submission-server/src/test/resources/fixtures/submission/dcc_root_dir/release1/project.5 \
- /tmp/DCC-1885/0.7a.json \
+ /tmp/DCC-1885/0.7e.json \
  /tmp/DCC-1885/project.5
 
 ./migration.sh \
  ../../../../dcc-submission/dcc-submission-server/src/test/resources/fixtures/submission/dcc_root_dir/release1/project.6 \
- /tmp/DCC-1885/0.7a.json \
+ /tmp/DCC-1885/0.7e.json \
  /tmp/DCC-1885/project.6
 
 ./migration.sh \
  ../../../../dcc-submission/dcc-submission-server/src/test/resources/fixtures/submission/dcc_root_dir/release1/project.7 \
- /tmp/DCC-1885/0.7a.json \
+ /tmp/DCC-1885/0.7e.json \
  /tmp/DCC-1885/project.7
 
 #rm -rf /tmp/DCC-1885
