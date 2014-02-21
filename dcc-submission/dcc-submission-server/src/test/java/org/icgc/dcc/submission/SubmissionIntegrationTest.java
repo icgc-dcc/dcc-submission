9a079b7fbe38886e8bc619064c23f3056da15389
Bob Tiernay <btiernay@oicr.on.ca>
2014-02-21 15:18:29 -0500

    Removed @Ignore from SIT to see if it works with Jenkins and Java 6 vs. Java 7.
    Former-commit-id: c27330a276bfd19b0b16051165ad473cf0f9d8ba
    Former-commit-id: ee8df8c4f230a7d681a545053cce6f39a26f13c9

--- dcc-submission/dcc-submission-server/src/test/java/org/icgc/dcc/submission/SubmissionIntegrationTest.java
+++ dcc-submission/dcc-submission-server/src/test/java/org/icgc/dcc/submission/SubmissionIntegrationTest.java
@@ -99,7 +99,6 @@ import org.icgc.dcc.submission.release.model.SubmissionState;
 import org.icgc.dcc.submission.sftp.Sftp;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -109,7 +108,6 @@ import com.dumbster.smtp.SimpleSmtpServer;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 
-@Ignore
 @Slf4j
 @RunWith(GuiceJUnitRunner.class)
 @GuiceModules({ ConfigModule.class, PersistenceModule.class })
