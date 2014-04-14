b5ed825bd9f31873177978a081f750e7c134ae32
Bob Tiernay <btiernay@oicr.on.ca>
2014-04-14 15:53:43 -0400

    Updated script to support deployment.
    Former-commit-id: 50f0c816feaecc4e4497fe18e36b16b4ede55268
    Former-commit-id: e0d8454a1c532e99e33c3d32877c8ea098ef3907

--- src/main/scripts/DCC-2177/build-reference-genome.sh
+++ src/main/scripts/DCC-2177/build-reference-genome.sh
@@ -3,14 +3,77 @@
 # Copyright 2014(c) The Ontario Institute for Cancer Research. All rights reserved.
 #
 # Description:
-#   Builds the reference genome based on https://wiki.oicr.on.ca/display/DCCSOFT/Unify+genome+assembly+build+throughout+the+system
+#   Builds and deploys the reference genome based on https://wiki.oicr.on.ca/display/DCCSOFT/Unify+genome+assembly+build+throughout+the+system
 #
 # Notes:
 #   Requires ncurses-dev zlib1g-dev
 # 
 # Usage:
-#   ./build-reference-genome.sh
+#   ./build-reference-genome.sh -u <username> -p -v <version>
 #
+# Download latest:
+#	curl -g 'http://seqwaremaven.oicr.on.ca/artifactory/dcc-dependencies/org/icgc/dcc/dcc-reference-genome/[RELEASE]/dcc-reference-genome-[RELEASE].tar.gz' | tar zvx
+#
+
+usage(){
+cat << EOF
+usage: $0 options
+
+This script downloads, formats, builds and deploys the GRCh37.fasta and GRCh37.fasta.fai tarball to Artifactory
+
+see https://jira.oicr.on.ca/browse/DCC-2177 for details
+
+OPTIONS:
+   -h      Show this message
+   -v      Artifactory version number (e.g. 1)
+   -u      Artifactory username
+   -p      Artifactory password
+EOF
+}
+
+log(){
+  echo `date` -  $1 | tee -a $logdir/install.log
+}
+
+version=
+username=
+password=
+while getopts “h:v:u:p:” OPTION
+do
+  case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         v)
+             version=$OPTARG
+             ;;
+         u)
+             username=$OPTARG
+             ;;
+         p)
+             read -s -p "Enter Password: " password
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+if [[ -z $version ]] || [[ -z $username ]] || [[ -z $password ]]
+then
+  usage
+  exit 1
+fi
+
+# Maven artifact location
+artifact_server=http://seqwaremaven.oicr.on.ca/artifactory
+artifact_repository=dcc-dependencies
+artifact_name=dcc-reference-genome
+artifact_path=org/icgc/dcc/$artifact_name
+artifact_version=GRCh37v$version
+archive_file=$artifact_name-$artifact_version.tar.gz
 
 # Ensembl: 1-22,X,MT
 ensemble_url=ftp://ftp.ensembl.org/pub/release-75/fasta/homo_sapiens/dna
@@ -56,3 +119,9 @@ done > fasta/GRCh37.fasta
 
 # Index
 samtools/samtools faidx fasta/GRCh37.fasta
+
+# Archive
+cd fasta; tar zcvf $archive_file *.fasta*; cd -
+
+# Deploy
+curl -X PUT -u$username:$password $artifact_server/$artifact_repository/$artifact_path/$artifact_version/$archive_file --data-binary @fasta/$archive_file
