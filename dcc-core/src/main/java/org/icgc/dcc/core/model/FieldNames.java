23743634c72a029efa62209003e718878e9869df
Bob Tiernay <btiernay@oicr.on.ca>
2014-05-02 17:13:04 -0400

    Added summarization to donor and gene FI summarization.
    Former-commit-id: 6172af7531d66bc9bc0434b5b540ba888c1f9359
    Former-commit-id: 3292355de571854f9cd99a068ab454e19f56b3b5

--- dcc-core/src/main/java/org/icgc/dcc/core/model/FieldNames.java
+++ dcc-core/src/main/java/org/icgc/dcc/core/model/FieldNames.java
@@ -196,6 +196,7 @@ public final class FieldNames {
   public static final String OBSERVATION_DONOR_ID = "_donor_id";
   public static final String OBSERVATION_PROJECT = "project";
   public static final String OBSERVATION_CONSEQUENCE_TYPES = "consequence_type";
+  public static final String OBSERVATION_FUNCTIONAL_IMPACT_PREDICTION_SUMMARY = "functional_impact_prediction_summary";
   public static final String OBSERVATION_CONSEQUENCES = "consequence";
   public static final String OBSERVATION_CONSEQUENCES_CONSEQUENCE_TYPE = "consequence_type";
   public static final String OBSERVATION_CONSEQUENCES_CONSEQUENCE_FUNCTIONAL_IMPACT_PREDICTION_SUMMARY =
