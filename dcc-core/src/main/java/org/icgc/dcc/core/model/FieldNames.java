bce990ce7b9d812bcb1839a959679080f1ca53e7
Bob Tiernay <btiernay@oicr.on.ca>
2014-05-02 15:24:38 -0400

    Added support for aggregation of FI in mutation centric. Factored out pathway transformation into base class. Changed ordering of FI classes.
    Former-commit-id: 1e83561c315f626e34589e6e323a09a82ff36de9
    Former-commit-id: 928ed8b59ccc2f0186c9afde8f86dd48688ed52a

--- dcc-core/src/main/java/org/icgc/dcc/core/model/FieldNames.java
+++ dcc-core/src/main/java/org/icgc/dcc/core/model/FieldNames.java
@@ -198,6 +198,10 @@ public final class FieldNames {
   public static final String OBSERVATION_CONSEQUENCE_TYPES = "consequence_type";
   public static final String OBSERVATION_CONSEQUENCES = "consequence";
   public static final String OBSERVATION_CONSEQUENCES_CONSEQUENCE_TYPE = "consequence_type";
+  public static final String OBSERVATION_CONSEQUENCES_CONSEQUENCE_FUNCTIONAL_IMPACT_PREDICTION_SUMMARY =
+      "functional_impact_prediction_summary";
+  public static final String OBSERVATION_CONSEQUENCES_CONSEQUENCE_FUNCTIONAL_IMPACT_PREDICTION =
+      "functional_impact_prediction";
   public static final String OBSERVATION_CONSEQUENCES_AA_MUTATION = CONSEQUENCE_AA_MUTATION;
   public static final String OBSERVATION_CONSEQUENCES_CONSEQUENCE_CANONICAL = "_is_canonical_transcript";
   public static final String OBSERVATION_CONSEQUENCES_TRANSCRIPT_ID = "_transcript_id";
@@ -230,6 +234,10 @@ public final class FieldNames {
 
   // TODO: Move to summary
   public static final String MUTATION_CONSEQUENCE_TYPES = "consequence_type";
+  public static final String MUTATION_FUNCTIONAL_IMPACT_PREDICTION_SUMMARY = "functional_impact_prediction_summary";
+  public static final String MUTATION_TRANSCRIPTS_FUNCTIONAL_IMPACT_PREDICTION_SUMMARY =
+      "functional_impact_prediction_summary";
+  public static final String MUTATION_TRANSCRIPTS_FUNCTIONAL_IMPACT_PREDICTION = "functional_impact_prediction";
   public static final String MUTATION_PLATFORM = "platform";
   public static final String MUTATION_IS_ANNOTATED = "is_annotated";
   public static final String MUTATION_VALIDATION_STATUS = "validation_status";
