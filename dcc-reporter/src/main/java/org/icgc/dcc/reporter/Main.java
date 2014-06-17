f5fe2323c2de7a497563724e344256643adfb9d0
Anthony Cros <anthony.cros@oicr.on.ca>
2014-06-17 10:44:19 -0400

    Refactored code that parses the JSON input file
    Former-commit-id: 1c4bd80d1cbea73b2d3160ac241156e975821f4a
    Former-commit-id: c4f068767d17a0869930babf38dffd8a24fc0548

--- dcc-reporter/src/main/java/org/icgc/dcc/reporter/Main.java
+++ dcc-reporter/src/main/java/org/icgc/dcc/reporter/Main.java
@@ -1,8 +1,14 @@
 package org.icgc.dcc.reporter;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
 
+  private static final String PARENT_DIR_PARAMETER = "parent_dir";
+
   public static void main(String[] args) {
+
     new Reporter().report("my_release", InputData.getDummy());
   }
 
