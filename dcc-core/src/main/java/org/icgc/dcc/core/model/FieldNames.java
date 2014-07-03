1d9cbbce43d706e2529e04e9e64065b7ca4e5427
mwdchang <mwdchang@gmail.com>
2014-07-03 14:40:18 -0400

    (indexer, portal-api, portal-ui) Add sequencing strategy facet, address typos and consistency
    Former-commit-id: b454ee17b8d473d6cc578951bc5f5d9ba2ff77ac
    Former-commit-id: 6c276f74fa0b9b7b5acb20123e50f88a2eca890e

--- dcc-core/src/main/java/org/icgc/dcc/core/model/FieldNames.java
+++ dcc-core/src/main/java/org/icgc/dcc/core/model/FieldNames.java
@@ -260,6 +260,7 @@ public final class FieldNames {
   public static final String OBSERVATION_VALIDATION_PLATFORM = "validation_platform";
   public static final String OBSERVATION_VERIFICATION_STATUS = "verification_status";
   public static final String OBSERVATION_VERIFICATION_PLATFORM = "verification_platform";
+  public static final String OBSERVATION_SEQUENCING_STRATEGY = "sequencing_strategy";
   public static final String OBSERVATION_IS_ANNOTATED = "is_annotated";
 
   public static String getPartitionTypeFieldName(String type) {
@@ -291,6 +292,7 @@ public final class FieldNames {
   public static final String MUTATION_IS_ANNOTATED = "is_annotated";
   public static final String MUTATION_VALIDATION_STATUS = "validation_status";
   public static final String MUTATION_VERIFICATION_STATUS = "verification_status";
+  public static final String MUTATION_SEQUENCING_STRATEGY = "sequencing_strategy";
 
   public static final String MUTATION_SUMMARY = "_summary";
   public static final String MUTATION_SUMMARY_AFFECTED_PROJECT_IDS = "_affected_project_id";
