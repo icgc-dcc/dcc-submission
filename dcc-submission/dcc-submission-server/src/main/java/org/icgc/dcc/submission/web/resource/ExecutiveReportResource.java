1d4493088d455878bd93f215ce4fd4e1903ce7d0
mwdchang <mwdchang@gmail.com>
2014-07-23 19:17:07 -0400

    (submission-server) first go for tsv executive reports
    Former-commit-id: 3af1abb1e6efea745ba87bd2fdb495aea1763094
    Former-commit-id: 7ee222907b484c76d5e6e382757431ff39772255

--- dcc-submission/dcc-submission-server/src/main/java/org/icgc/dcc/submission/web/resource/ExecutiveReportResource.java
+++ dcc-submission/dcc-submission-server/src/main/java/org/icgc/dcc/submission/web/resource/ExecutiveReportResource.java
@@ -32,7 +32,11 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.icgc.dcc.submission.service.ExecutiveReportService;
 
+import com.google.common.base.Joiner;
 import com.google.common.base.Objects;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 
 @Slf4j
@@ -42,26 +46,84 @@ public class ExecutiveReportResource {
   @Inject
   private ExecutiveReportService service;
 
+  private final Joiner joiner = Joiner.on("\t");
+
   @GET
   @Path("projectDataType/{releaseName}")
-  @Produces("application/json")
+  @Produces("text/plain")
   public Response getProjectDataTypeReport(
       @PathParam("releaseName") String releaseName,
       @QueryParam("projects") List<String> projects) {
 
     val reports = service.getProjectDataTypeReport(releaseName,
         Objects.firstNonNull(projects, Collections.<String> emptyList()));
-    return Response.ok(reports).build();
+
+    List<String> header = ImmutableList.<String> builder().add(
+        "Release", "Project Id", "Type").build();
+
+    val result = new StringBuilder();
+
+    result.append(joiner.join(header));
+    result.append("\n");
+
+    for (val report : reports) {
+      val line = Lists.<String> newArrayList();
+      line.add(report.getReleaseName());
+      line.add(report.getProjectCode());
+      line.add(report.getType());
+      result.append(joiner.join(line));
+      result.append("\n");
+    }
+
+    return Response.ok(result.toString()).build();
   }
 
   @GET
   @Path("projectSequencingStrategy/{releaseName}")
-  @Produces("application/json")
+  @Produces("text/plain")
   public Response getProjectSequencingStrategyReport(
       @PathParam("releaseName") String releaseName,
       @QueryParam("projects") List<String> projects) {
+
     val reports = service.getProjectSequencingStrategyReport(releaseName,
         Objects.firstNonNull(projects, Collections.<String> emptyList()));
-    return Response.ok(reports).build();
+    val staticHeader = ImmutableList.<String> of("Release", "Project Id");
+
+    val result = new StringBuilder();
+
+    // Headers may vary across releases, but within a specific release we expect to have
+    // the same header for all its projects since they use the same dictionary and codelists.
+    // If not, we have bigger problems...
+    if (!reports.isEmpty()) {
+      val dynamicHeader =
+          ImmutableSortedSet.<String> naturalOrder().addAll(reports.get(0).getCountSummary().keySet()).build();
+      val header = ImmutableList.<String> builder().addAll(staticHeader).addAll(dynamicHeader).build();
+      result.append(joiner.join(header));
+      result.append("\n");
+
+      for (val report : reports) {
+        List<String> line = Lists.newArrayList();
+
+        // Handle static
+        line.add(report.getReleaseName());
+        line.add(report.getProjectCode());
+
+        // Handle dynamic
+        for (val key : dynamicHeader) {
+          line.add(report.getCountSummary().get(key).toString());
+        }
+        result.append(joiner.join(line));
+        result.append("\n");
+      }
+
+    } else {
+      result.append(joiner.join(staticHeader));
+      result.append("\n");
+    }
+
+    // reports.get(0).getCountSummary().keySet()
+    // List<String> dynamicHeader =
+
+    return Response.ok(result.toString()).build();
   }
 }
