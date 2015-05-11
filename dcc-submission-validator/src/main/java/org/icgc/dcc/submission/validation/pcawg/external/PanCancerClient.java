28deae29127b4243e137e110b6f00e33983208e5
Bob Tiernay <btiernay@oicr.on.ca>
2015-05-11 10:56:32 -0400

    Added blacklisted filter to sample ids.
    Former-commit-id: 45bf8c528397800ffc21b0d7340722c75281b19b

--- dcc-submission-validator/src/main/java/org/icgc/dcc/submission/validation/pcawg/external/PanCancerClient.java
+++ dcc-submission-validator/src/main/java/org/icgc/dcc/submission/validation/pcawg/external/PanCancerClient.java
@@ -135,6 +135,8 @@ public class PanCancerClient {
   }
 
   private JsonNode searchDonors(String... fields) {
+    val excludeBlacklisted =
+        "{\"query\":{\"bool\":{\"must_not\":[{\"term\":{\"flags.is_manual_qc_failed\":\"T\"}},{\"term\":{\"flags.is_donor_blacklisted\":\"T\"}}]}}}";
     return search("donor/_search"
         + "?"
         + "size"
@@ -143,7 +145,11 @@ public class PanCancerClient {
         + "&"
         + "fields"
         + "="
-        + COMMA.join(fields));
+        + COMMA.join(fields)
+        + "&"
+        + "source"
+        + "="
+        + excludeBlacklisted);
   }
 
   @SneakyThrows
