574476723e7320f224c94be2d562bcccd2cd3255
Chang Wang <garnwraly@gmail.com>
2016-08-22 17:01:19 -0400

    Add react-router

--- dcc-submission-ui-new/src/index.js
+++ dcc-submission-ui-new/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Router, Route, browserHistory } from 'react-router'
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+  ), document.getElementById('root')
 );
