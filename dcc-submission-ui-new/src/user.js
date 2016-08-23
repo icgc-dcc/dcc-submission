ba0c44bcf90567a12d0f15acb9beda3f17503d19
Chang Wang <garnwraly@gmail.com>
2016-08-23 16:08:53 -0400

    minor: whitespace on user

--- dcc-submission-ui-new/src/user.js
+++ dcc-submission-ui-new/src/user.js
@@ -25,7 +25,7 @@ user.login = action('login', async function (username, password) {
   runInAction('update login status', () => { this.isLoggingIn = false });
 
   if (response.status === 200) {
-    const userData = await response.json(); 
+    const userData = await response.json();
     runInAction('update user data', () => {
       this.username = userData.username;
       this.roles = userData.roles;
