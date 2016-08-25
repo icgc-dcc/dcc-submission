521998da5b803bba1bae8748ef3ddb28b098760f
Chang Wang <garnwraly@gmail.com>
2016-08-25 16:21:10 -0400

    Use Breadcrumbs component

--- dcc-submission-ui-new/src/App.js
+++ dcc-submission-ui-new/src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+import Breadcrumbs from './common/components/Breadcrumbs/Breadcrumbs';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Breadcrumbs pathName={this.props.location.pathname}/>
         {this.props.children}
       </div>
     );
