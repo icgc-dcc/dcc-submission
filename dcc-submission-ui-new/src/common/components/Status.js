2a4bab53ce1f1cec01ee35311f4fe6c9bfbc7645
Chang Wang <garnwraly@gmail.com>
2016-08-29 17:37:09 -0400

    add Status component

---
+++ dcc-submission-ui-new/src/common/components/Status.js
@@ -0,0 +1,52 @@
+import React, {PropTypes} from 'react';
+
+const STATUS_MAP = {
+  NOT_VALIDATED: {
+    wrapperClass: '',
+    iconClass: 'fa fa-question-circle',
+  },
+  ERROR: {
+    wrapperClass: 'error',
+    iconClass: 'fa fa-exclamation-circle',
+  },
+  INVALID: {
+    wrapperClass: 'error',
+    iconClass: 'fa fa-minus-circle',
+  },
+  QUEUED: {
+    wrapperClass: 'queued',
+    iconClass: 'fa fa-clock-o',
+  },
+  VALIDATING: {
+    wrapperClass: 'validating',
+    iconClass: 'fa fa-cogs',
+  },
+  VALID: {
+    wrapperClass: 'valid',
+    iconClass: 'fa fa-check-circle',
+  },
+  SIGNED_OFF: {
+    wrapperClass: 'valid',
+    iconClass: 'fa fa-lock',
+  },
+  _DEFAULT: {
+    wrapperClass: '',
+    iconClass: '',
+  }
+};
+
+function Status ({statusCode}) {
+  const { wrapperClass, iconClass } = STATUS_MAP[statusCode] || STATUS_MAP._DEFAULT;
+  return (
+    <span className={wrapperClass}>
+      <i className={iconClass}/>
+      {(statusCode || 'SKIPPED').replace(/_/g, ' ')}
+    </span>
+  );
+}
+
+Status.propTypes = {
+  statusCode: PropTypes.string.isRequired,
+};
+
+export default Status;
