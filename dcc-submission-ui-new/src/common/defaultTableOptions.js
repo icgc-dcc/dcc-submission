c175bdb5633137440697292419145a95344dd9f7
Chang Wang <garnwraly@gmail.com>
2016-08-30 15:38:52 -0400

    bugfix: pagination “to” value needs to self-correct

--- dcc-submission-ui-new/src/common/defaultTableOptions.js
+++ dcc-submission-ui-new/src/common/defaultTableOptions.js
@@ -1,6 +1,6 @@
 export default {
   sizePerPage: 20,
-  paginationShowsTotal: (start, to, total) => `Showing ${start + 1} to ${to + 1} of ${total + 1} entries`,
+  paginationShowsTotal: (start, to, total) => `Showing ${start + 1} to ${Math.min(to + 1, total)} of ${total} entries`,
   hideSizePerPage: true,
   defaultSortOrder: 'asc',
 };
