1a500eed24af22e3cd34f34153695ed53759cd46
Chang Wang <garnwraly@gmail.com>
2016-08-31 17:09:09 -0400

    Layout:minor: add bootstrap’s `container` class to App

--- dcc-submission-ui-new/src/App.js
+++ dcc-submission-ui-new/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
         <Breadcrumbs
           routes={this.props.routes}
           params={this.props.params}
