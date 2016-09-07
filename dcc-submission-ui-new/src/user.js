daa8dd5d9e61bf54029aa54dd4529a66fa383c56
Chang Wang <garnwraly@gmail.com>
2016-09-07 17:07:30 -0400

    Add logout method to user

--- dcc-submission-ui-new/src/user.js
+++ dcc-submission-ui-new/src/user.js
@@ -1,4 +1,4 @@
-import {observable, action, runInAction} from 'mobx';
+import {observable, action} from 'mobx';
 
 import {fetchHeaders} from '~/utils'; 
 
@@ -17,33 +17,37 @@ const user = observable({
   isAdmin: function () {
     return this.roles.indexOf('admin') >= 0;
   },
-});
 
-user.login = action('login', async function (username, password) {
-  this.isLoggingIn = true;
-  const token = generateToken(username, password);
-  const response = await fetch('/ws/users/self', {
-      headers: {
-        ...fetchHeaders.get(),
-        Authorization: `Basic ${token}`,
-      }
-    });
-
-  runInAction('update login status', () => { this.isLoggingIn = false });
-
-  if (response.status === 200) {
-    const userData = await response.json();
-    runInAction('update user data', () => {
+  login: action(async function (username, password) {
+    this.isLoggingIn = true;
+    const token = generateToken(username, password);
+    const response = await fetch('/ws/users/self', {
+        headers: {
+          ...fetchHeaders.get(),
+          Authorization: `Basic ${token}`,
+        }
+      });
+    if (response.status === 200) {
+      this.isLoggingIn = false;
+      const userData = await response.json();
       this.username = userData.username;
       this.roles = userData.roles;
       this.token = token;
       this.isLoggedIn = true;
-    }); 
-  } else if (response.status === 401) {
-    throw new Error('Incorrect username or password');
-  } else {
-    throw new Error('Login failed');
-  }
+    } else if (response.status === 401) {
+      throw new Error('Incorrect username or password');
+    } else {
+      throw new Error('Login failed');
+    }
+  }),
+
+  logout: action(function () {
+    this.isLoggedIn = false;
+    this.username = '';
+    this.token = '';
+    this.emailsToNotify = [];
+    this.roles = [];
+  })
 });
 
 export default user;
