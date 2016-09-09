de6afad2ac3c4a020a4ec8151215c3a2690913cd
Chang Wang <garnwraly@gmail.com>
2016-09-09 18:13:19 -0400

    add className “username” to username in header

--- dcc-submission-ui-new/src/Header/Header.js
+++ dcc-submission-ui-new/src/Header/Header.js
@@ -70,7 +70,7 @@ class Header extends Component {
               <ul className="nav navbar-nav navbar-right">
                 <li></li>
                 <li>
-                  <a>{user.username}</a>
+                  <a className="username">{user.username}</a>
                 </li>
                 <li>
                   <a style={{cursor: 'pointer'}} onClick={() => user.logout()}>Logout</a>
