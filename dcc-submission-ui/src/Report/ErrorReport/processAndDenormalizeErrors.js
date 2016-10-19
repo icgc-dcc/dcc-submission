d0bec2b31577fe7549d07532e9d17f13f4f06d12
Chang Wang <cheapsteak@gmail.com>
2016-10-19 11:29:20 -0400

    bugfix: Account for fieldNames having`null` value (#41)

--- dcc-submission-ui/src/Report/ErrorReport/processAndDenormalizeErrors.js
+++ dcc-submission-ui/src/Report/ErrorReport/processAndDenormalizeErrors.js
@@ -6,9 +6,10 @@ export default function processAndDenormalizeErrors (errorReports) {
   return flatten(errorReports.map( errorReport => errorReport.fieldErrorReports.map(
     fieldErrorReport => ({
       ...fieldErrorReport,
-      errorType: errorReport.errorType, 
+      errorType: errorReport.errorType,
+      fieldNames: fieldErrorReport.fieldNames || [],
       lineValueMap: zipObject(fieldErrorReport.lineNumbers, fieldErrorReport.values),
       lineNumbers: fieldErrorReport.lineNumbers.sort((a, b) => a - b),
-      key: `${errorReport.errorType}&${fieldErrorReport.fieldNames.join('&')}`,
+      key: `${errorReport.errorType}&${(fieldErrorReport.fieldNames || []).join('&')}`,
     }))));
 };
